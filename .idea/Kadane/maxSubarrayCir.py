a16cbdadd0b9df480def31561775cebcf225b628
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-08-23 23:10:25 -0400

    done with maxSubarrayCir.py

--- .idea/Kadane/maxSubarrayCir.py
+++ .idea/Kadane/maxSubarrayCir.py
@@ -1,6 +1,11 @@
 class Solution:
     def maxSubarraySumCircular(self, nums: list[int]) -> int:
-        return max(self.maxSubArray(nums), self.withoutNeg(nums));
+        total = sum(nums);
+        maximum = self.maxSubArray(nums);
+        minimum = self.minSubArray(nums);
+        if total-minimum <= 0:
+            return maximum;
+        return max(maximum, total-minimum);
 
     def maxSubArray(self, nums: list[int]) -> int:
         res = nums[0];
@@ -13,17 +18,17 @@ class Solution:
                 sum = 0;
         return res;
 
-    def withoutNeg(self, nums: list[int]) -> int:
-        total = sum(nums);
-        maximum = total;
-        newTotal = total;
+    def minSubArray(self, nums: list[int]) -> int:
+        res = nums[0];
+        sum = 0;
         for i in range(len(nums)):
-            if newTotal > (newTotal - nums[i]):
-                maximum = max(newTotal, maximum);
-                newTotal = total;
-            else:
-                newTotal -= nums[i];
-        return maximum;
+            sum += nums[i];
+            if sum < res:
+                res = sum;
+            if sum > 0:
+                sum = 0;
+        return res;
 
 sol = Solution();
-print(sol.withoutNeg([-2,4,-5,4,-5,9,4]))
+print(sol.maxSubarraySumCircular([-2,4,-5,4,-5,9,4]));
+print(sol.maxSubarraySumCircular([-3,-2,-3]));
