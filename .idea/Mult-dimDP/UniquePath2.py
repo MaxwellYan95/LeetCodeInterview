8cd7f88f3cb9e8b5234ddb04fa2fb0b48e74bf1a
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-08-24 22:36:38 -0400

    attempting UniquePath2.py AGAIN

--- .idea/Mult-dimDP/UniquePath2.py
+++ .idea/Mult-dimDP/UniquePath2.py
@@ -1,22 +1,31 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
         dp = [[-1 for j in obstacleGrid[0]] for i in obstacleGrid];
-        def recur(x, y) -> int:
-            if (x == len(obstacleGrid[0])-1 and y == len(obstacleGrid)-1):
-                return 1;
-            if dp[y][x] != -1:
-                return dp[y][x];
-            if (obstacleGrid[y][x] == 1):
-                dp[y][x] = 0;
-            down = 0;
-            right = 0;
-            if y+1 > len(obstacleGrid)-1:
-                down = recur(x, y+1);
-            if x+1 > len(obstacleGrid[0])-1:
-                right = recur(x+1, y);
-            dp[y][x] = down + right;
-            return down + right;
-        return recur(0, 0);
+        maxCol = len(obstacleGrid[0])-1;
+        maxRow = len(obstacleGrid)-1;
+        for i in range(len(obstacleGrid)):
+            element = dp[i][maxCol];
+            spot = obstacleGrid[i][maxCol];
+            if spot == 1:
+                dp[i][maxCol] = 0;
+            else:
+                dp[i][maxCol] = 1;
+        for j in range(len(obstacleGrid[0])):
+            element = dp[maxRow][j];
+            spot = obstacleGrid[maxRow][j];
+            if spot == 1:
+                dp[maxRow][j] = 0;
+            else:
+                dp[maxRow][j] = 1;
+        for i in range(len(obstacleGrid)-2, -1, -1):
+            for j in range(len(obstacleGrid[0])-2, -1, -1):
+                element = dp[i][j];
+                spot = obstacleGrid[i][j];
+                if spot == 1:
+                    dp[i][j] = 0;
+                else:
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+        return dp[0][0];
 sol = Solution();
 print(sol.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]));
 
