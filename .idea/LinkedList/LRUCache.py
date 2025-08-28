11422cd26de8a0067412af42fedb861c1a345da4
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-08-27 23:30:07 -0400

    attempting LRUCache

--- .idea/LinkedList/LRUCache.py
+++ .idea/LinkedList/LRUCache.py
@@ -13,7 +13,23 @@ class LRUCache:
         self.capacity = capacity;
 
 
-    # def get(self, key: int) -> int:
+    def get(self, key: int) -> int:
+        if self.cache == None:
+            return -1;
+        if (self.cache.key == key):
+            return self.cache.value;
+        def traverse(prev: Node, cur: Node):
+            return traverse(cur, cur.next);
+            if cur.key == key:
+                res = cur.value;
+                prev.next = cur.next;
+                return res;
+            if cur.next == None:
+                return -1;
+        result = traverse(self.cache, self.cache.next);
+        if result != -1:
+            first = self.cache;
+            self.cache = LRUCache()
 
 
     def put(self, key: int, value: int) -> None:
@@ -26,6 +42,9 @@ class LRUCache:
             while traverse.next != None and counter != self.capacity:
                 traverse = traverse.next;
                 counter += 1;
+                if traverse.key == key:
+                    traverse.value = value;
+                    return;
             traverse.next = Node(key, value);
             if counter == self.capacity:
                 self.cache = first.next;
@@ -35,4 +54,5 @@ sol.put(1, 1);
 sol.put(2, 2);
 sol.put(3, 3);
 sol.put(4, 4);
+sol.put(3, 4);
 sol.put(5, 5);
