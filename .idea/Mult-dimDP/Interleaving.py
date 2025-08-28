c913175291a375812629fc1d77c5139445c8c0ac
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-08-27 22:52:08 -0400

    attempting Interleaving.py AGAIN

--- .idea/Mult-dimDP/Interleaving.py
+++ .idea/Mult-dimDP/Interleaving.py
@@ -1,20 +1,31 @@
 class Solution:
-    class Solution:
-        def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-            def recurInterleave(s1: str, s2: str, s3: str) -> bool:
-                if (len(s1) == 0):
-                    return s3 == s2;
-                elif (len(s2) == 0):
-                    return s3 == s1;
-                elif (s1[0] == s3[0] and s2[0] == s1[0]):
-                    return recurInterleave(s1[1:], s2, s3[1:]) or recurInterleave(s1, s2[1:], s3[1:]);
-                elif (s1[0] == s3[0]):
-                    return recurInterleave(s1[1:], s2, s3[1:]);
-                elif (s2[0] == s3[0]):
-                    return recurInterleave(s1, s2[1:], s3[1:]);
-                else:
-                    return False;
-            return recurInterleave(s1, s2, s3);
+    def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
+        dp = [[False for i in range(len(s2)+1)] for j in range(len(s3)+1)]
+        def recurInterleave(s1: str, s2: str, s3: str) -> bool:
+            if (dp[len(s3)][len(s2)] == True):
+                return True;
+            elif (len(s1) == 0):
+                dp[len(s3)][len(s2)] = (s3 == s2);
+                return s3 == s2;
+            elif (len(s2) == 0):
+                dp[len(s3)][len(s2)] = (s3 == s1);
+                return s3 == s1;
+            elif (len(s1) == 0):
+                dp[len(s3)][len(s2)] = (s3 == s2);
+                return s3 == s2;
+            elif (s1[0] == s3[0] and s2[0] == s1[0]):
+                dp[len(s3)][len(s2)] = (recurInterleave(s1[1:], s2, s3[1:]) or recurInterleave(s1, s2[1:], s3[1:]));
+                return dp[len(s3)][len(s2)];
+            elif (s1[0] == s3[0]):
+                dp[len(s3)][len(s2)] = recurInterleave(s1[1:], s2, s3[1:]);
+                return dp[len(s3)][len(s2)];
+            elif (s2[0] == s3[0]):
+                dp[len(s3)][len(s2)] = recurInterleave(s1, s2[1:], s3[1:]);
+                return dp[len(s3)][len(s2)];
+            else:
+                dp[len(s3)][len(s2)] = False;
+            return False;
+        return recurInterleave(s1, s2, s3);
 sol = Solution();
 print(sol.isInterleave("a", "b", "a"));
 
