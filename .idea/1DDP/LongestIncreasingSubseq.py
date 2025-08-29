bcdda1e4758b80a7e214be985c1268f09b7cef43
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-08-29 14:46:06 -0400

    completed longestIncreasingSubseq.py

--- .idea/1DDP/LongestIncreasingSubseq.py
+++ .idea/1DDP/LongestIncreasingSubseq.py
@@ -1,16 +1,11 @@
 class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
-        while nums[0] > nums[1]:
-            nums = nums[1:];
-        while nums[len(nums)-2] > nums[len(nums)-1]:
-            nums = nums[:len(nums)-1];
-        i = 1;
-        while True:
-            cond = (num[i-1] > nums[i]) or (num[i] > nums[i+1]);
-            if cond:
-                nums = nums[:i] + nums[i+1:];
-            else:
-                i += 1;
-            if i == len(nums)-1:
-                break;
-        return len(nums);
+        results = [1 for n in nums];
+        for index in range(len(nums)-2, -1, -1):
+            for inner in range(index+1, len(nums)):
+                if nums[index] < nums[inner]:
+                    results[index] = max(1+results[inner], results[index]);
+        return max(results);
+
+sol = Solution();
+print(sol.lengthOfLIS([10,9,2,5,3,7,101,18]));
