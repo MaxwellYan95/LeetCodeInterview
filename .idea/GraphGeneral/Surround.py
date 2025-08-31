c304baa85ffff374153436d0525d7053b5af3ad3
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-08-31 11:12:57 -0400

    solved surround.py

--- .idea/GraphGeneral/Surround.py
+++ .idea/GraphGeneral/Surround.py
@@ -1,55 +1,38 @@
 import collections;
 
 class Solution:
-    def adjacent(self, x: int, y: int, maxX: int, maxY: int):
-        adj = [];
-        if (y != 0):
-            adj.append([x, y-1]);
-        if (y != maxY):
-            adj.append([x, y+1]);
-        if (x != 0):
-            adj.append([x-1, y]);
-        if (x != maxX):
-            adj.append([x+1, y]);
-        return adj;
     def solve(self, board: list[list[str]]) -> None:
-        visited = [["X" for inner in board[0]] for outer in board];
-        stack = collections.deque();
-        def bfs(startX: int, startY: int):
-            stack.append([startX, startY]);
-            while stack:
-                x, y = stack.popleft();
-                visited[y][x] = "O";
-                adj = self.adjacent(x, y, len(board[0])-1, len(board)-1);
-                for x1, y1 in adj:
-                    hasO = (board[y1][x1] == "O")
-                    notVisited = (visited[y1][x1] == "X");
-                    if hasO and notVisited:
-                        stack.append([x1, y1]);
+        def dfs(x: int, y: int):
+            if (x < 0 or y < 0 or x >= len(board[0]) or y >= len(board) or board[y][x] != "O"):
+                return;
+            board[y][x] = "#";
+            dfs(x-1, y);
+            dfs(x+1, y);
+            dfs(x, y-1);
+            dfs(x, y+1);
 
         for y in range(len(board)):
             hasO = (board[y][0] == "O");
-            notVisited = (visited[y][0] == "X");
-            if hasO and notVisited:
-                bfs(0, y);
+            if hasO:
+                dfs(0, y);
             hasO = (board[y][len(board[0])-1] == "O");
-            notVisited = (visited[y][len(board[0])-1] == "X");
-            if hasO and notVisited:
-                bfs(len(board[0])-1, y);
+            if hasO:
+                dfs(len(board[0])-1, y);
 
         for x in range(len(board[0])):
             hasO = (board[0][x] == "O");
-            notVisited = (visited[0][x] == "X");
-            if hasO and notVisited:
-                bfs(x, 0);
+            if hasO:
+                dfs(x, 0);
             hasO = (board[len(board)-1][x] == "O");
-            notVisited = (visited[len(board)-1][x] == "X");
-            if hasO and notVisited:
-                bfs(x, len(board)-1);
+            if hasO:
+                dfs(x, len(board)-1);
 
-        for y in range(len(visited)):
-            for x in range(len(visited[0])):
-                board[y][x] = visited[y][x];
+        for y in range(len(board)):
+            for x in range(len(board[0])):
+                if board[y][x] == "#":
+                    board[y][x] = "O";
+                else:
+                    board[y][x] = "X";
 
 
 
