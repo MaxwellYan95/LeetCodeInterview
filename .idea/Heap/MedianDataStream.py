62986fda2b617410b41dd90c74362c4c0b8eab95
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-08-31 13:27:04 -0400

    using heappq

--- .idea/Heap/MedianDataStream.py
+++ .idea/Heap/MedianDataStream.py
@@ -1,44 +1,28 @@
+
+
+import heapq;
+
 class MedianFinder:
 
     def __init__(self):
         self.numbers = [];
+        heapq.heapify(self.numbers);
 
     def addNum(self, num: int) -> None:
-        if len(self.numbers) == 0:
-            self.numbers.append(num);
-            return;
-        size = len(self.numbers);
-        mid = int(size/2);
-        while True:
-            if mid == len(self.numbers)-1:
-                if self.numbers[mid] < num:
-                    self.numbers = self.numbers + [num];
-                else:
-                    self.numbers = self.numbers[1:(size-1)] + [num] + self.numbers[size-1:];
-                break;
-            elif mid == 0:
-                if num < self.numbers[mid]:
-                    self.numbers = [num] + self.numbers;
-                else:
-                    self.numbers = self.numbers[0] + [num] + self.numbers[1:];
-                break;
-            elif self.numbers[mid-1] < num < self.numbers[mid]:
-                self.numbers = self.numbers[:mid] + [num] + self.numbers[mid:];
-                break;
-            elif self.numbers[mid-1] > num:
-                mid = int(mid/2);
-            elif num > self.numbers[mid]:
-                mid += int(mid/2);
+        heapq.heappush(self.numbers, num);
 
     def findMedian(self) -> float:
         mid = int(len(self.numbers)/2);
+        temp = heapq.nsmallest(mid+1, self.numbers);
         if len(self.numbers) % 2 == 0:
-            return float(self.numbers[mid] + self.numbers[mid-1]) / 2;
+            return float(temp[mid-1] + temp[mid]) / 2;
         else:
-            return self.numbers[mid];
+            return temp[mid];
 
 median = MedianFinder();
 median.addNum(1);
 median.addNum(2);
 print(median.findMedian());
 median.addNum(3);
+median.addNum(4);
+print(median.findMedian());
