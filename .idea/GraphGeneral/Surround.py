bb1f3e519cbb70ee9c986aa11f2baa134d58c726
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-08-30 20:22:41 -0400

    attempting surround.py

--- .idea/GraphGeneral/Surround.py
+++ .idea/GraphGeneral/Surround.py
@@ -1,39 +1,49 @@
+import collections
 
-def solve(board: list[list[str]]) -> None:
-    """
-    Do not return anything, modify board in-place instead.
-    """
-    visited = []
-    newGrid = []
-    for x in range(len(board)):
-        visitRow = []
-        newGridRow = []
-        for y in range(len(board[0])):
-            visitRow.append(False)
-            newGridRow.append(board[x][y])
-        visited.append(visitRow)
-        newGrid.append(newGridRow)
+class Solution:
+    def adjacent(self, x: int, y: int, maxX: int, maxY: int):
+        adj = [];
+        if (y != 0):
+            adj.append([x, y-1]);
+        if (y != maxY):
+            adj.append([x, y+1]);
+        if (x != 0):
+            adj.append([x-1, y]);
+        if (x != maxX):
+            adj.append([x+1, y]);
+        return adj;
+    def solve(self, board: list[list[str]]) -> None:
+        visited = [["0" for inner in board[0]] for outer in board];
+        def dfs(x: int, y: int, edge: bool) -> bool:
+            condList = [];
+            onEdge = (x == 0 or x == len(board[0])-1 or y == 0 or y == len(board)-1);
+            adj = self.adjacent(x, y, len(board[0])-1, len(board)-1);
+            visited[y][x] = "X";
+            for x1, y1 in adj:
+                hasO = (board[y1][x1] == "O")
+                notVisited = (visited[y1][x1] != "X");
+                if hasO and notVisited:
+                    condList.append(dfs(x1, y1, onEdge));
+            if False not in condList and edge != True and onEdge != True:
+                for x1, y1 in adj:
+                    board[y1][x1] = "X";
+                return True;
+            return False;
+        for y in range(len(board)):
+            for x in range(len(board[y])):
+                hasO = (board[y][x] == "O")
+                notVisited = (visited[y][x] != "X");
+                if hasO and notVisited:
+                    dfs(x, y, False);
+        return board;
 
-    def identify(board: list[list[str]], x: int, y: int) -> bool:
-        counter = False
-        if ((x == 0 or x == len(board)-1 or y == 0 or y == len(board[0])-1) and board[x][y] == "O"):
-            counter = True
-        elif (board[x][y] == "X"):
-            visited[x][y] = True
-            return False
-        elif (visited[x][y] == True):
-            return False
-        visited[x][y] = True
-        edge = counter or identify(board, x+1, y) or identify(board, x-1, y) or identify(board, x, y+1) or identify(board, x, y-1)
-        if (edge == False and board[x][y] == "O"):
-            board[x][y] = "X"
-        return edge
 
-    for x in range(1, len(board)-1):
-        for y in range(1, len(board[0])-1):
-            if (visited[x][y] == False):
-                identify(board, x, y)
-            else:
-                visited[x][y] = True
-
-print(solve([["O","O","O","O","X","X"],["O","O","O","O","O","O"],["O","X","O","X","O","O"],["O","X","O","O","X","O"],["O","X","O","X","O","O"],["O","X","O","O","O","O"]]))
+sol = Solution();
+print(sol.solve([["O","O","O","O","X","X"],
+             ["O","O","O","O","O","O"],
+             ["O","X","O","X","O","O"],
+             ["O","X","O","O","X","O"],
+             ["O","X","O","X","O","O"],
+             ["O","X","O","O","O","O"]]))
+print(sol.solve([["X","X","X","X"],["X","O","O","X"],["X","X","O","X"],["X","O","X","X"]]))
+print(sol.solve([["X","X","X"],["X","O","X"],["X","X","X"]]));
