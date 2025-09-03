8615daa2cb4bc2bd9cbed32479814ad71e6e7082
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-03 16:30:41 -0400

    planning on doing LRUcache later

--- .idea/LinkedList/LRUCache.py
+++ .idea/LinkedList/LRUCache.py
@@ -3,73 +3,34 @@ class Node:
         self.key = key;
         self.value = value;
         self.next = None;
-    def setKey(self, key: int):
-        self.key = key;
-    def setValue(self, value: int):
-        self.value = value;
-
-
+        self.prev = None;
 
 class LRUCache:
 
     def __init__(self, capacity: int):
-        self.cache = None;
+        self.front = None;
+        self.end = None;
         self.capacity = capacity;
         self.length = 0;
 
+    #def get(self, key: int) -> int:
 
-    def get(self, key: int) -> int:
-        if self.cache == None:
-            return -1;
-        if (self.cache.key == key):
-            return self.cache.value;
-        def traverse(prev: Node, cur: Node):
-            if cur == None:
-                return -1;
-            if cur.key == key:
-                oldKey = cur.key;
-                res = cur.value;
-                prev.next = cur.next;
-                oldCache = self.cache;
-                self.cache = Node(oldKey, res);
-                self.cache.next = oldCache;
-                return res;
-            if cur.next == None:
-                return -1;
-            return traverse(cur, cur.next);
-        result = traverse(self.cache, self.cache.next);
-        return result;
-
-    # removes last element
-    def removeLast(self):
-        if self.cache.next == None:
-            self.cache = None;
-        else:
-            traverse = self.cache;
-            for i in range(self.capacity-2):
-                traverse = traverse.next;
-            traverse.next = None;
 
     def put(self, key: int, value: int) -> None:
-        if self.cache == None:
-            self.cache = Node(key, value);
-            self.length += 1;
-            return;
-        if self.get(key):
-            self.cache.value = value;
-            return;
-        if self.length == self.capacity:
-            self.removeLast();
+        if self.front == None:
+            self.front = Node(key, value);
+            self.back = Node(key, value);
+            self.front.next = self.back;
+            self.front.prev = self.back;
+            self.back.next = self.front;
+            self.back.prev = self.front;
         else:
-            self.length += 1;
-        first = self.cache;
-        self.cache = Node(key, value);
-        self.cache.next = first;
+            oldFront = self.front;
+            self.front = Node(key, value);
+            self.front.next = oldFront;
+            self.front.prev = self.back;
 
 sol = LRUCache(2);
 sol.put(2, 1);
-sol.put(1, 1);
-sol.put(2, 3);
-sol.put(4, 1);
-print(sol.get(1));
-print(sol.get(2));
+sol.put(3, 2);
+
