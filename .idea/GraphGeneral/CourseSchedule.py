afa20cc6c7988363b555bfae956c37654f4b11bf
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-02 23:06:37 -0400

    ORDER OF IF CONDITIONS MATTERS

--- .idea/GraphGeneral/CourseSchedule.py
+++ .idea/GraphGeneral/CourseSchedule.py
@@ -1,38 +1,32 @@
-import collections
-
+from collections import defaultdict
 class Solution:
     def makeDict(self, prerequisites: list[list[int]]):
-        graph = dict();
+        graph = defaultdict(list);
         for c1, c2 in prerequisites:
-            if c1 not in graph:
-                graph[c1] = [[c1, c2]];
-            else:
-                graph[c1].append([c1, c2]);
+            graph[c1].append(c2);
         return graph;
 
     def canFinish(self, numCourses: int, prerequisites: list[list[int]]) -> bool:
         preqDict = self.makeDict(prerequisites);
         visited = [];
-        def dfs(edge: list[int]):
-            course = edge[1];
+        def dfs(course: int):
+            # Dont put "course in visited" first
             if course in visited:
                 return False;
-            visited.append(course);
-            if course not in preqDict:
+            if not preqDict[course]:
                 return True;
-            for nextEdge in preqDict[course]:
-                result = dfs(nextEdge);
-                if result == False:
+            visited.append(course);
+            for subCourse in preqDict[course]:
+                if not dfs(subCourse):
                     return False;
+            preqDict.pop(course);
             return True;
-        for key in preqDict.keys():
-            for edge in preqDict[key]:
-                res = dfs(edge);
-                if res == False:
-                    return False;
-                visited = [];
+        for course in range(numCourses):
+            if not dfs(course):
+                return False;
         return True;
 
 sol = Solution();
 print(sol.canFinish(2, [[1,0],[0,1]]));
 print(sol.canFinish(5, [[1,4],[2,4],[3,1],[3,2]]));
+print(sol.canFinish(3, [[0,1],[0,2],[1,2]]));
