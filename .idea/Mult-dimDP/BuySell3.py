2cb74019199d1ba40f7bcdad93b1cf6d14737047
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-03 18:27:00 -0400

    attempting BuySell3.py

--- .idea/Mult-dimDP/BuySell3.py
+++ .idea/Mult-dimDP/BuySell3.py
@@ -16,13 +16,20 @@ class Solution:
                 prevDP = dp[begin][end-1];
                 curMinimum = minimums[begin];
                 dp[begin][end] = max(prices[end]-curMinimum, dp[begin][end-1]);
-        return dp
+        return dp;
 
 
-
-
-    #def maxProfit(self, prices: list[int]) -> int:
+    def maxProfit(self, prices: list[int]) -> int:
+        maximum = 0;
+        dp = self.createChart(prices);
+        for end1 in range(len(dp[0])):
+            trans1 = dp[0][end1];
+            beg2 = end1+1;
+            for end2 in range(beg2+1, len(dp[0])):
+                trans2 = dp[beg2][end2];
+                maximum = max(trans2+trans1, maximum);
+        return maximum;
 
 sol = Solution();
 grid = sol.createChart([3,3,5,0,0,3,1,4]);
-print();
+print(sol.maxProfit([3,3,5,0,0,3,1,4]));
