40706a50628090ce049df6c7425a4d0b73705bbe
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-03 22:19:45 -0400

    tried to clear up more memory

--- .idea/Mult-dimDP/BuySell3.py
+++ .idea/Mult-dimDP/BuySell3.py
@@ -1,13 +1,16 @@
+from collections import defaultdict;
 class Solution:
 
     # Max profit for one transactions
-    def createChart(self, prices: list[int]) -> int:
-        dp = [[0 for j in range(len(prices))] for i in range(len(prices))];
+    def createChart(self, prices: list[int]) -> dict:
+        dp = defaultdict(int);
         for begin in range(len(prices)-1):
             minimum = prices[begin];
             for end in range(begin+1, len(prices)):
-                prevDP = dp[begin][end-1];
-                dp[begin][end] = max(prices[end]-minimum, dp[begin][end-1]);
+                prevDP = dp[(begin, end-1)];
+                betterDP = prices[end]-minimum;
+                if prevDP > 0 or betterDP > 0:
+                    dp[(begin, end)] = max(prevDP, betterDP);
                 minimum = min(minimum, prices[end]);
         return dp;
 
@@ -15,14 +18,14 @@ class Solution:
     def maxProfit(self, prices: list[int]) -> int:
         maximum = 0;
         dp = self.createChart(prices);
-        for end1 in range(1, len(dp[0])):
-            trans1 = dp[0][end1];
+        for end1 in range(1, len(prices)):
+            trans1 = dp[(0, end1)];
             beg2 = end1+1;
-            if end1 == len(dp[0])-1:
+            if end1 == len(prices)-1:
                 maximum = max(trans1, maximum);
             else:
-                for end2 in range(beg2+1, len(dp[0])):
-                    trans2 = dp[beg2][end2];
+                for end2 in range(beg2+1, len(prices)):
+                    trans2 = dp[(beg2, end2)];
                     maximum = max(trans2+trans1, maximum);
         return maximum;
 
@@ -30,3 +33,4 @@ sol = Solution();
 grid = sol.createChart([3,3,5,0,0,3,1,4]);
 print(sol.maxProfit([3,3,5,0,0,3,1,4]));
 print(sol.maxProfit([1,2,3,4,5]));
+print(sol.maxProfit([1, 2]));
