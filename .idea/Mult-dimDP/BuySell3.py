e4c26e6ed606e351a4806d8546aeaf80fd7f02f7
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-03 22:20:35 -0400

    found simple solution to BuySell3.py, but I need to understand it

--- .idea/Mult-dimDP/BuySell3.py
+++ .idea/Mult-dimDP/BuySell3.py
@@ -1,33 +1,17 @@
 from collections import defaultdict;
 class Solution:
 
-    # Max profit for one transactions
-    def createChart(self, prices: list[int]) -> dict:
-        dp = defaultdict(int);
-        for begin in range(len(prices)-1):
-            minimum = prices[begin];
-            for end in range(begin+1, len(prices)):
-                prevDP = dp[(begin, end-1)];
-                betterDP = prices[end]-minimum;
-                if prevDP > 0 or betterDP > 0:
-                    dp[(begin, end)] = max(prevDP, betterDP);
-                minimum = min(minimum, prices[end]);
-        return dp;
-
-
     def maxProfit(self, prices: list[int]) -> int:
-        maximum = 0;
-        dp = self.createChart(prices);
-        for end1 in range(1, len(prices)):
-            trans1 = dp[(0, end1)];
-            beg2 = end1+1;
-            if end1 == len(prices)-1:
-                maximum = max(trans1, maximum);
-            else:
-                for end2 in range(beg2+1, len(prices)):
-                    trans2 = dp[(beg2, end2)];
-                    maximum = max(trans2+trans1, maximum);
-        return maximum;
+        transaction1, transaction2 = float('inf'), float('inf')
+        profit1, profit2 = 0, 0
+
+        for i in prices:
+            transaction1 = min(transaction1, i)
+            profit1 = max(profit1, i - transaction1)
+
+            transaction2 = min(transaction2, i-profit1)
+            profit2 = max(profit2, i - transaction2)
+        return profit2
 
 sol = Solution();
 grid = sol.createChart([3,3,5,0,0,3,1,4]);
