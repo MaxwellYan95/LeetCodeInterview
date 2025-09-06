5abb6cf0803029e4fda59fb2893732b60efaed0c
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-06 01:06:05 -0400

    I used tuples in KthSmallest.py. However, there's a time exceeded error

--- .idea/Heap/KthSmallest.py
+++ .idea/Heap/KthSmallest.py
@@ -7,14 +7,16 @@ class Solution:
         visited = [];
         heapq.heapify(h);
         heapq.heappush(h, [nums1[0]+nums2[0], 0, 0]);
+        visited.append((0, 0));
         while kCounter < k and h:
             sum, i, j = heapq.heappop(h);
             result.append([nums1[i], nums2[j]]);
-            visited.append([i, j]);
-            if i+1 < len(nums1) and [i+1, j] not in visited:
+            if i+1 < len(nums1) and (i+1, j) not in visited:
                 heapq.heappush(h, [nums1[i+1]+nums2[j], i+1, j]);
-            if j+1 < len(nums2) and [i, j+1] not in visited:
+                visited.append((i+1, j));
+            if j+1 < len(nums2) and (i, j+1) not in visited:
                 heapq.heappush(h, [nums1[i]+nums2[j+1], i, j+1]);
+                visited.append((i, j+1));
             kCounter += 1;
         return result;
 
