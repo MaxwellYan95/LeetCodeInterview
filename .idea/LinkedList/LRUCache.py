5f69f5fc6c96cdbf737b060bc281eced638f303b
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-06 13:25:15 -0400

    creating add and remove funct

--- .idea/LinkedList/LRUCache.py
+++ .idea/LinkedList/LRUCache.py
@@ -15,13 +15,46 @@ class LRUCache:
 
 
     def put(self, key: int, value: int) -> None:
+        if self.length != self.capacity:
+            self.add(key, value);
+        else:
+            contained = self.get(key);
+            if (contained == -1):
+                self.delete(self.back);
+                self.back = self.back.prev;
+                self.add(key, value);
+            else:
+                self.delete(self.front);
+                self.front = self.front.next;
+                self.add(key, contained);
+
+    def get(self, key: int) -> int:
+        for i in range(self.length):
+            if (self.front.key == self.back.key):
+                self.back = self.back.prev;
+            if self.front.key == key:
+                return self.front.value;
+            else:
+                self.front = self.front.next;
+        return -1;
+
+    def delete(self, location: Node):
+        if self.length == 1:
+            self.front = None;
+            self.back = None;
+        else:
+            location.prev.next = location.next;
+            location.next.prev = location.prev;
+        self.length -= 1;
+
+    def add(self, key: int, value: int):
         if self.front == None:
             self.front = Node(key, value);
             self.back = self.front;
             self.front.next = self.back;
             self.front.prev = self.back;
             self.length += 1;
-        elif self.length != self.capacity:
+        else:
             oldFront = self.front;
             self.front = Node(key, value);
             oldFront.prev = self.front;
@@ -29,30 +62,17 @@ class LRUCache:
             self.back.next = self.front;
             self.front.prev = self.back;
             self.length += 1;
-        else:
-            contained = self.get(key);
-            if contained != -1:
-                self.front.value = value;
-            else:
-                self.front = self.front.prev;
-                self.back = self.back.prev;
-                self.front.key = key;
-                self.front.value = value;
-
-    def get(self, key: int) -> int:
-        for i in range(self.capacity):
-            if self.front.key == key:
-                return self.front.value;
-            else:
-                self.front = self.front.next;
-                self.back = self.back.next;
-        return -1;
 
 
 
 
 sol = LRUCache(2);
-sol.put(2, 1);
-sol.put(3, 2);
-sol.put(5, 4);
-print();
+sol.put(1, 1);
+sol.put(2, 2);
+print(sol.get(1));
+sol.put(3, 3);
+print(sol.get(2));
+sol.put(4, 4);
+print(sol.get(1));
+print(sol.get(3));
+print(sol.get(4));
