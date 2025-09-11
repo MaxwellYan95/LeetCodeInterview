c10f3f93c0b1f1c2f2f0ab988ef59aaaf42d49a1
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-11 15:25:05 -0400

    completed KthSmallest.py AGAIN

--- .idea/Heap/KthSmallest.py
+++ .idea/Heap/KthSmallest.py
@@ -4,11 +4,22 @@ class Solution:
         minH = [];
         results = [];
         heapq.heapify(minH);
-        for n1 in nums1:
-            for n2 in nums2:
-                heapq.heappush(minH, [n1+n2, (n1, n2)]);
-        for _ in range(k):
-            results.append(heapq.heappop(minH)[1]);
+        index1 = 0;
+        index2 = 0;
+        heapq.heappush(minH, [nums1[index1]+nums2[index2], index1, index2])
+        visited = set();
+        visited.add((0,0));
+        counter = 0;
+        while counter != k:
+            addedVal, i1, i2 = heapq.heappop(minH);
+            results.append((nums1[i1], nums2[i2]));
+            if i1+1 < len(nums1) and (i1+1, i2) not in visited:
+                heapq.heappush(minH, [nums1[i1+1]+nums2[i2], i1+1, i2])
+                visited.append((i1+1, i2))
+            if i2+1 < len(nums2) and (i1, i2+1) not in visited:
+                heapq.heappush(minH, [nums1[i1]+nums2[i2+1], i1, i2+1])
+                visited.append((i1, i2+1))
+            counter += 1;
         return results;
 
 sol = Solution();
