bb119b44bb6e3567818147a7a36234bbfad5608c
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-11 15:11:57 -0400

    tried KthSmallest.py AGAIN but this solution takes TOO LONG

--- .idea/Heap/KthSmallest.py
+++ .idea/Heap/KthSmallest.py
@@ -1,24 +1,15 @@
 import heapq;
 class Solution:
     def kSmallestPairs(self, nums1: list[int], nums2: list[int], k: int) -> list[list[int]]:
-        h = [];
-        result = [];
-        kCounter = 0;
-        visited = set();
-        heapq.heapify(h);
-        heapq.heappush(h, [nums1[0]+nums2[0], 0, 0]);
-        visited.append((0, 0));
-        while kCounter < k and h:
-            sum, i, j = heapq.heappop(h);
-            result.append([nums1[i], nums2[j]]);
-            if i+1 < len(nums1) and (i+1, j) not in visited:
-                heapq.heappush(h, [nums1[i+1]+nums2[j], i+1, j]);
-                visited.append((i+1, j));
-            if j+1 < len(nums2) and (i, j+1) not in visited:
-                heapq.heappush(h, [nums1[i]+nums2[j+1], i, j+1]);
-                visited.append((i, j+1));
-            kCounter += 1;
-        return result;
+        minH = [];
+        results = [];
+        heapq.heapify(minH);
+        for n1 in nums1:
+            for n2 in nums2:
+                heapq.heappush(minH, [n1+n2, (n1, n2)]);
+        for _ in range(k):
+            results.append(heapq.heappop(minH)[1]);
+        return results;
 
 sol = Solution();
 print(sol.kSmallestPairs([1,2,4,5,6], [3,5,7,9], 20));
