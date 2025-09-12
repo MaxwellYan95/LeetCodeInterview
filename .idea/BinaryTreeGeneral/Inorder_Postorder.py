65f566ed649ceb530fa2faa57512e28d707b6036
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-12 15:11:44 -0400

    completed Inorder_Postorder.py (REMEMBER KEEP THE CODE AND LOGIC SIMPLE IF YOU CAN)

--- .idea/BinaryTreeGeneral/Inorder_Postorder.py
+++ .idea/BinaryTreeGeneral/Inorder_Postorder.py
@@ -7,32 +7,14 @@ class TreeNode:
         self.left = left
         self.right = right
 class Solution:
-    def makeDict(self, inorder: list[int]):
-        indexDict = defaultdict(int);
-        for index in range(len(inorder)):
-            indexDict[inorder[index]] = index;
-        return indexDict;
-
     def buildTree(self, inorder: list[int], postorder: list[int]) -> TreeNode:
-        self.index = len(postorder)-1;
-        indexDict = self.makeDict(inorder);
-        def recur(inorder: list[int], counter: int):
-            if self.index == 0:
-                return TreeNode(postorder[0]);
-            value = postorder[self.index];
-            nextIndex = indexDict[value] - counter;
-            if len(inorder) == 1:
-                return TreeNode(value);
-            rightTree = None;
-            leftTree = None;
-            self.index = self.index-1;
-            if self.index >= 0:
-                rightTree = recur(inorder[nextIndex+1:], nextIndex+1);
-            self.index = self.index-1;
-            if self.index >= 0:
-                leftTree = recur(inorder[:nextIndex], -nextIndex+1);
-            return TreeNode(value, leftTree, rightTree);
-        return recur(inorder, 0);
+        if len(inorder) == 0 or len(postorder) == 0:
+            return None;
+        value = postorder.pop();
+        nextIndex = inorder.index(value);
+        rightTree = self.buildTree(inorder[nextIndex+1:], postorder);
+        leftTree = self.buildTree(inorder[:nextIndex], postorder);
+        return TreeNode(value, leftTree, rightTree);
 
 sol = Solution();
 result1 = sol.buildTree([9,3,15,20,7], [9,15,7,20,3]);
