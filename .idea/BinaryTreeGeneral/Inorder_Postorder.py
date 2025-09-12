17913908c9c93c5d7879344d6dac59dcb29bdb3b
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-12 13:28:52 -0400

    working on Inorder_Postorder.py

---
+++ .idea/BinaryTreeGeneral/Inorder_Postorder.py
@@ -0,0 +1,39 @@
+from collections import defaultdict;
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def makeDict(self, inorder: list[int]):
+        indexDict = defaultdict(int);
+        for index in range(len(inorder)):
+            indexDict[inorder[index]] = index;
+        return indexDict;
+
+    def buildTree(self, inorder: list[int], postorder: list[int]) -> TreeNode:
+        self.index = len(postorder)-1;
+        indexDict = self.makeDict(inorder);
+        def recur(inorder: list[int], counter: int):
+            if self.index == 0:
+                return TreeNode(postorder[0]);
+            value = postorder[self.index];
+            nextIndex = indexDict[value] - counter;
+            if len(inorder) == 1:
+                return TreeNode(value);
+            rightTree = None;
+            leftTree = None;
+            if self.index >= 0:
+                self.index = self.index-1;
+                rightTree = recur(inorder[nextIndex+1:], nextIndex+1);
+            if self.index >= 0:
+                self.index = self.index-1;
+                leftTree = recur(inorder[:nextIndex], -nextIndex+1);
+            return TreeNode(value, leftTree, rightTree);
+        return recur(inorder, 0);
+
+sol = Solution();
+result = sol.buildTree([9,3,15,20,7], [9,15,7,20,3]);
+print();
