35fbbd11b6a4bc58e378040d1df151636084312b
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-09-13 11:38:17 -0400

    completed IPO.py (read the problem carefully)

--- .idea/Heap/IPO.py
+++ .idea/Heap/IPO.py
@@ -1,32 +1,29 @@
-import collections
+import heapq;
 
 
 class Solution:
+    # IMPORTANT NOTE
+    # ===============
+    # The problem never mentioned anything about the order of an array.
+    # The problem said to pick k projects from the list such that you
+    # make the maximum profit. You start off with w capital.
     def findMaximizedCapital(self, k: int, w: int, profits: list[int], capital: list[int]) -> int:
-        stack = collections.deque();
-        curW = w;
-        curK = 0;
-        maximum = 0;
-        for index in range(len(profits)):
-            curProfit = profits[index];
-            capRequire = capital[index];
-            if curK == k:
-                insideLoop = False;
-                while curW > capRequire and stack:
-                    insideLoop = True;
-                    freeUp = stack.pop();
-                    curW -= freeUp;
-                    curK -= 1;
-                if insideLoop and curW < capRequire:
-                    stack.append(freeUp);
-                    curW += freeUp;
-                    curK += 1;
-            if curW >= capRequire and curK < k:
-                stack.append(curProfit);
-                curW += curProfit;
-                curK += 1;
-                maximum = max(curW, maximum);
-        return maximum;
+        stack = [];
+        n = len(profits);
+        tupList = [(capital[i], profits[i]) for i in range(n)];
+        tupList.sort();
+        index = 0;
+        heapq.heapify(stack);
+        for _ in range(k):
+            # Order of the "and" condition in a while loop matters
+            while index < n and w >= tupList[index][0]:
+                heapq.heappush(stack, -tupList[index][1]);
+                index += 1;
+            if not stack:
+                break;
+            w -= heapq.heappop(stack);
+        return w;
+
 
 
 
