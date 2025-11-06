4b7116d05a4efdf6026d1e3c7fd990f50c913487
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-11-06 13:45:45 -0500

    did KthSmallest.py again

--- .idea/Heap/KthSmallest.py
+++ .idea/Heap/KthSmallest.py
@@ -1,26 +1,29 @@
 import heapq;
 class Solution:
     def kSmallestPairs(self, nums1: list[int], nums2: list[int], k: int) -> list[list[int]]:
-        minH = [];
-        results = [];
-        heapq.heapify(minH);
-        index1 = 0;
-        index2 = 0;
-        heapq.heappush(minH, [nums1[index1]+nums2[index2], index1, index2])
-        visited = set();
-        visited.add((0,0));
-        counter = 0;
-        while counter != k:
-            addedVal, i1, i2 = heapq.heappop(minH);
-            results.append((nums1[i1], nums2[i2]));
-            if i1+1 < len(nums1) and (i1+1, i2) not in visited:
-                heapq.heappush(minH, [nums1[i1+1]+nums2[i2], i1+1, i2])
-                visited.append((i1+1, i2))
-            if i2+1 < len(nums2) and (i1, i2+1) not in visited:
-                heapq.heappush(minH, [nums1[i1]+nums2[i2+1], i1, i2+1])
-                visited.append((i1, i2+1))
-            counter += 1;
-        return results;
+        minH = [[nums2[0]+nums1[0], (0, 0)]]
+        visited = set()
+        results = []
+        visited.add((0, 0))
+        heapq.heapify(minH)
+        while len(results) != k:
+            total, coord = heapq.heappop(minH)
+            i1 = coord[0]
+            i2 = coord[1]
+            results.append([nums1[i1], nums2[i2]])
+            bounds1 = (i1+1) < len(nums1)
+            bounds2 = (i2+1) < len(nums2)
+            if (i1+1, i2) not in visited and bounds1:
+                heapq.heappush(minH, [nums2[i2]+nums1[i1+1], (i1+1, i2)])
+                visited.add((i1+1, i2))
+            if (i1, i2+1) not in visited and bounds2:
+                heapq.heappush(minH, [nums2[i2+1]+nums1[i1], (i1, i2+1)])
+                visited.add((i1, i2+1))
+        return results
+
+
 
 sol = Solution();
-print(sol.kSmallestPairs([1,2,4,5,6], [3,5,7,9], 20));
+lst = [[1,1]];
+print([1,1] not in lst);
+print(sol.kSmallestPairs([1,7,11], [2,4,6], 7));
