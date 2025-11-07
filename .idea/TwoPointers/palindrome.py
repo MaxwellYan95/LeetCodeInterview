a606cde03cad279b0fc2ad7c5cff901a70e0d096
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-11-07 12:02:27 -0500

    finished palindrome.py

--- .idea/TwoPointers/palindrome.py
+++ .idea/TwoPointers/palindrome.py
@@ -1,19 +1,24 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        lowerS = s.lower();
+        newS = self.changeFormat(s)
         p1 = 0;
-        p2 = len(lowerS)-1;
-        while p1 < p2 and p1 < len(lowerS) and p2 >= 0:
-            while p1 < len(lowerS)-1 and not lowerS[p1].isalpha():
-                p1 = p1 + 1;
-            while p2 >= 1 and not lowerS[p2].isalpha():
-                p2 = p2 - 1;
-            if lowerS[p1] != lowerS[p2] and lowerS[p1].isalpha() and lowerS[p2].isalpha():
+        p2 = len(newS)-1;
+        while p1 < p2 and p1 < len(newS) and p2 >= 0:
+            if newS[p1] != newS[p2]:
                 return False;
             p1 = p1 + 1;
             p2 = p2 - 1;
         return True;
 
+    def changeFormat(self, s: str) -> str:
+        result = ""
+        for letter in s:
+            if letter.isalnum():
+                result += letter;
+        return result.lower();
+
 sol = Solution();
 print(sol.isPalindrome("A man, a plan, a canal: Panama"))
 print(sol.isPalindrome(".,"))
+print(sol.isPalindrome(".,"))
+
