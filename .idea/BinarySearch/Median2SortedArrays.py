0ff97f6124c335e95c6ab7122062e7fb6cfcc26f
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-11-07 17:34:23 -0500

    solved Median2SortedArrays.py

--- .idea/BinarySearch/Median2SortedArrays.py
+++ .idea/BinarySearch/Median2SortedArrays.py
@@ -2,33 +2,36 @@ import math
 
 class Solution:
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        nums1.insert(0, -(math.inf-1));
-        nums2.insert(0, -(math.inf-1));
-        nums1.append(math.inf)
-        nums2.append(math.inf)
-        i1 = (len(nums1)-1)//2;
-        i2 = (len(nums2)-1)//2;
-        if (len(nums1) == len(nums2)):
-            i2 = i2 - 1;
-        while i1 < len(nums1)-1 or i2 < len(nums2)-1 or i1 > 0 or i2 > 0:
-            left1 = nums1[i1];
-            right1 = nums1[i1+1];
-            left2 = nums2[i2];
-            right2 = nums2[i2+1];
-            print(f"nums1 = {nums1}");
-            print(f"nums2 = {nums2}");
-            print(f"l1 = {left1}, r1 = {right1}, l2 = {left2}, r2 = {right2}");
-            if right2 < left1:
-                i2 = i2 + 1;
-                i1 = i1 - 1;
-            elif left2 > right1:
-                i2 = i2 - 1;
-                i1 = i1 + 1;
+        A=nums1
+        B=nums2
+
+        if len(B)<len(A):
+            A,B=B,A
+
+        total=len(A)+len(B)
+        half=total//2
+
+        l=0
+        r=len(A)-1
+
+        while True:
+            a=(r+l)//2
+            b=half-a-2
+            Aleft=A[a]   if a>=0 else float("-inf")
+            Aright=A[a+1] if (a+1)<len(A) else float("inf")
+            Bleft=B[b] if b>=0 else float("-inf")
+            Bright=B[b+1] if (b+1)< len(B) else float("inf")
+
+            if Aleft <= Bright and Bleft<=Aright:
+                if total%2:
+                    return min(Aright,Bright)
+                return (max(Aleft,Bleft)+ min(Aright,Bright))/2
+
+            elif Aleft>Bright:
+                r=a-1
+
             else:
-                break;
-        if (len(nums1)+len(nums2)) % 2 == 0:
-            return (min(right1, right2)+max(left1, left2)) / 2
-        return max(left1, left2);
+                l=a+1
 
 sol = Solution();
 print(sol.findMedianSortedArrays([1, 3, 4], [2, 5]));
