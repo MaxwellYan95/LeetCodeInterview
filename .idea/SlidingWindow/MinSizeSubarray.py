a704f39c34660f73d1f098d89a4f5b4f4553a8ff
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-11-09 18:02:26 -0500

    runtime was exceeded

--- .idea/SlidingWindow/MinSizeSubarray.py
+++ .idea/SlidingWindow/MinSizeSubarray.py
@@ -2,21 +2,19 @@ import statistics
 import sys
 
 class Solution:
-    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         lower = 0;
         upper = 1;
         total = 0;
         minLen = sys.maxsize;
         while lower != upper and upper <= len(nums):
             window = nums[lower:upper];
+            total = sum(window)
             if total < target:
-                total += nums[upper-1]
                 upper += 1
             else:
-                while total >= target and lower != upper:
-                    minLen = min(upper-lower, minLen)
-                    total -= nums[lower]
-                    lower += 1
+                minLen = min(upper-lower, minLen)
+                lower += 1
         if minLen == sys.maxsize:
             return 0;
         return minLen;
