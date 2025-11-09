3855d05449e252b0bc2b15c453c6594bf7e911c7
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2025-11-09 18:40:18 -0500

    solved MinSizeSubarray.py

--- .idea/SlidingWindow/MinSizeSubarray.py
+++ .idea/SlidingWindow/MinSizeSubarray.py
@@ -2,24 +2,17 @@ import statistics
 import sys
 
 class Solution:
-    def minSubArrayLen(self, target: int, nums: list[int]) -> int:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
         lower = 0;
-        upper = 1;
         total = 0;
         minLen = sys.maxsize;
-        while lower != upper and upper <= len(nums):
-            window = nums[lower:upper];
-            total = sum(window)
-            if total < target:
-                upper += 1
-            else:
-                minLen = min(upper-lower, minLen)
+        for upper in range(len(nums)):
+            total += nums[upper]
+            while total >= target:
+                minLen = min(upper-lower+1, minLen);
+                total -= nums[lower]
                 lower += 1
         if minLen == sys.maxsize:
             return 0;
         return minLen;
-
-sol = Solution()
-print(sol.minSubArrayLen(4, [1,4,4]))
-
 
