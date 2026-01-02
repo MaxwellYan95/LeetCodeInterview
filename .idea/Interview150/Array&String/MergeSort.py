086454dc45b2ed3df14561bdbcae3813d0811953
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-02 18:40:52 -0500

    finished attempt of MergeSort.py. Started at the end of nums1 since nums1 is longer. (Looked at code)

--- .idea/Interview150/Array&String/MergeSort.py
+++ .idea/Interview150/Array&String/MergeSort.py
@@ -1,28 +1,24 @@
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
-        if m == 0:
-            return nums2[:n];
         if n == 0:
-            return nums1[:m];
-        index1 = 0;
-        index2 = 0;
-        ind = 0;
-        n1 = nums1[:n];
-        while (index2 < n or index1 < m):
-            if (index1 == m):
+            return nums1;
+        index1 = m-1;
+        index2 = n-1;
+        ind = m+n-1;
+        while (index2 >= 0 or index1 >= 0):
+            if index1 < 0:
                 nums1[ind] = nums2[index2];
-                index2 += 1;
-            elif (index2 == n):
-                nums1[ind] = n1[index1];
-                index1 += 1;
+                index2 -= 1;
+            elif index2 < 0:
+                nums1[ind] = nums1[index1];
+                index1 -= 1;
+            elif nums1[index1] > nums2[index2]:
+                nums1[ind] = nums1[index1];
+                index1 -= 1;
             else:
-                if n1[index1] >= nums2[index2]:
-                    nums1[ind] = nums2[index2];
-                    index2 += 1;
-                else:
-                    nums1[ind] = n1[index1];
-                    index1 += 1;
-            ind += 1;
+                nums1[ind] = nums2[index2];
+                index2 -= 1;
+            ind -= 1;
 
 sol = Solution();
 print(sol.merge([0], 0, [1], 1));
