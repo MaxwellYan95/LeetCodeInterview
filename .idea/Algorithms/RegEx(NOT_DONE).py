3b74db5036da534519dcdf97e32f058e5f721572
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-02 14:19:08 -0500

    NOT DONE WITH REG EX

--- .idea/Algorithms/RegEx(NOT_DONE).py
+++ .idea/Algorithms/RegEx(NOT_DONE).py
@@ -1,6 +1,6 @@
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
-        matrix = [[False for inner in range(len(p))] for out in range(len(s))];
+        matrix = [[False for inner in range(len(p)+1)] for out in range(len(s)+1)];
 
         # Initializes matrix[0][0]
         if p[0] == '*' or p[0] == '.' or p[0] == s[0]:
@@ -9,27 +9,25 @@ class Solution:
             matrix[0][0] = False;
 
         # Initializes matrix[0][s]
-        for x in range(1, len(s)):
+        for x in range(1, len(s)+1):
             if p[0] == '*':
                 matrix[x][0] = True;
             else:
                 matrix[x][0] = False;
         for x in range(len(s)):
-            for y in range(1, len(p)):
+            for y in range(1, len(p)+1):
                 if y == 0 and x == 0:
                     continue;
                 elif p[y] == '.':
-                    if (x-1 < 0):
+                    if x-1 < 0:
                         matrix[x][y] = False;
                     else:
                         matrix[x][y] = matrix[x-1][y-1];
                 elif p[y] == '*':
-                    if (x-1 < 0 and y-2 < 0):
+                    if p[y-1] == '.':
                         matrix[x][y] = True;
-                    elif (y-2 >= 0):
-                        matrix[x][y] = matrix[x][y-2] or matrix[x-1][y];
                     else:
-                        matrix[x][y] = matrix[x-1][y];
+                        matrix[x][y] = s[x] == p[y-1];
                 elif p[y] == s[x]:
                     matrix[x][y] = matrix[x][y-1];
                 else:
