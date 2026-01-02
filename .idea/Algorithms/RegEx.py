9e4025270b09230278943a002db622f35564eb03
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-02 10:09:00 -0500

    attempting Regular Expression

---
+++ .idea/Algorithms/RegEx.py
@@ -0,0 +1,33 @@
+class Solution:
+    # def isMatch(self, s: str, p: str) -> bool:
+
+    # Examples
+    # 1. Changes .*.. to ...*
+    # 2. Changes **. to .*
+
+    def simplifyEx(self, p: str) -> str:
+        newP = "";
+        lstP = list(p);
+        index = 0;
+        while index != len(lstP):
+            curP = lstP[index];
+            periods = 0;
+            hasStar = False;
+            while (self.isReg(curP) and index != len(lstP)):
+                if self.isPeriod(curP):
+                    periods += 1;
+                elif self.isStar(curP):
+                    hasStar = True;
+        return newP;
+
+    def isStar(self, p: str) -> bool:
+        return p == '*';
+
+    def isPeriod(self, p: str) -> bool:
+        return p == '.';
+
+    def isReg(self, p: str) -> bool:
+        return self.isStar(p) or self.isPeriod(p);
+
+sol = Solution();
+sol.simplifyEx("a*...")
