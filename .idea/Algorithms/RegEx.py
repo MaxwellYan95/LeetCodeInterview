043ba6dc50c14c601c6930e2127a137b8a77c4a5
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-02 10:49:30 -0500

    created simplifyEx(), MIGHT NOT USE IT

--- .idea/Algorithms/RegEx.py
+++ .idea/Algorithms/RegEx.py
@@ -1,23 +1,33 @@
 class Solution:
     # def isMatch(self, s: str, p: str) -> bool:
 
+
     # Examples
     # 1. Changes .*.. to ...*
     # 2. Changes **. to .*
-
     def simplifyEx(self, p: str) -> str:
         newP = "";
         lstP = list(p);
         index = 0;
-        while index != len(lstP):
+        while index < len(lstP):
             curP = lstP[index];
             periods = 0;
             hasStar = False;
-            while (self.isReg(curP) and index != len(lstP)):
-                if self.isPeriod(curP):
-                    periods += 1;
-                elif self.isStar(curP):
-                    hasStar = True;
+            if self.isReg(curP):
+                while (self.isReg(curP) and index < len(lstP)):
+                    if self.isPeriod(curP):
+                        periods += 1;
+                    elif self.isStar(curP):
+                        hasStar = True;
+                    index += 1;
+                    curP = lstP[min(index, len(lstP)-1)];
+                for p in range(periods):
+                    newP += (".");
+                if hasStar:
+                    newP += ("*")
+            else:
+                newP += (curP + "")
+                index += 1;
         return newP;
 
     def isStar(self, p: str) -> bool:
@@ -30,4 +40,4 @@ class Solution:
         return self.isStar(p) or self.isPeriod(p);
 
 sol = Solution();
-sol.simplifyEx("a*...")
+print(sol.simplifyEx("*.**..aaa..***.*..*"))
