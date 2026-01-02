b0adb848b34d5692782cf7a3d885a42a7205564c
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-02 14:50:53 -0500

    solved atoi

---
+++ .idea/Algorithms/Atoi.py
@@ -0,0 +1,56 @@
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        digits = self.getDigits(s);
+        place = 10 ** (len(digits)-2);
+        sign = 1;
+        if (len(digits) == 0):
+            return 0;
+        if (digits[0] == '-'):
+            sign = -1;
+        sum = 0;
+        for d in digits[1:]:
+            sum += int(d)*place;
+            place = place // 10;
+        sum *= sign;
+        if (sum < -(2**31)):
+            return -(2**31)
+        elif (sum > 2**31-1):
+            return 2**31-1
+        return sum
+
+    def getDigits(self, s: str) -> list[str]:
+        s = s.strip();
+        lst = [];
+        starts = False;
+        for char in s:
+            if starts:
+                if not self.isDigit(char):
+                    break;
+                else:
+                    lst.append(char)
+            else:
+                if (char == '-'):
+                    lst.append('-');
+                    starts = True;
+                elif (char == '+'):
+                    lst.append('+');
+                    starts = True;
+                elif (self.isDigit(char)):
+                    lst.append('+');
+                    lst.append(char);
+                    starts = True;
+                elif (char != ' '):
+                    break;
+        return lst;
+
+
+    def isDigit(self, s: str) -> bool:
+        return s == '0' or s == '1' or s == '2' or s == '3' \
+               or s == '4' or s == '5' or s == '6' or s == '7'\
+                or s == '8' or s == '9'
+
+    def isSign(self, s: str) -> bool:
+        return s == '+' or s == '-'
+
+sol = Solution();
+print(sol.myAtoi("   -1337c0d3"));
