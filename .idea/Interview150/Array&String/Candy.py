e9561eb37cd037477d4e9de6087bb407bee48841
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-02 20:50:20 -0500

    completed Candy.py without any difficulty (oddly)

--- .idea/Interview150/Array&String/Candy.py
+++ .idea/Interview150/Array&String/Candy.py
@@ -2,37 +2,19 @@ class Solution:
     def candy(self, ratings: list[int]) -> int:
         if len(ratings) <= 1:
             return len(ratings)*1;
-        leftRate = ratings[0];
-        rightRate = ratings[len(ratings)-1]
-        candies = 0;
-        if (leftRate > ratings[1]):
-            candies += 2;
-        else:
-            candies += 1;
-        if (rightRate > ratings[len(ratings)-2]):
-            candies += 2;
-        else:
-            candies += 1;
-        for index in range(0, len(ratings)):
-            if index == 0:
-                if ratings[index] > ratings[index+1]:
-                    candies += 2;
-                else:
-                    candies += 1;
-                continue
-            elif index == len(ratings)-1:
-                if ratings[index] > ratings[index-1]:
-                    candies += 2;
-                else:
-                    candies += 1;
-                continue
-            equal = (ratings[index] >= ratings[index-1] and ratings[index] >= ratings[index+1]);
-            greater = (ratings[index] > ratings[index-1] or ratings[index] > ratings[index+1])
-            if greater and equal:
-                candies += 2;
-            else:
-                candies += 1;
-        return candies;
+        left = 0;
+        right = len(ratings)-1;
+        candies = [1]*len(ratings);
+        while left < len(ratings)-1:
+            if ratings[left] < ratings[left+1]:
+                candies[left+1] = candies[left] + 1;
+            left += 1
+        while right > 0:
+            notGreater = not (candies[right] < candies[right-1])
+            if ratings[right] < ratings[right-1] and notGreater:
+                candies[right-1] = candies[right] + 1;
+            right -= 1;
+        return sum(candies);
 
 sol = Solution()
-print(sol.candy())
+print(sol.candy([1, 3, 4, 5, 2]))
