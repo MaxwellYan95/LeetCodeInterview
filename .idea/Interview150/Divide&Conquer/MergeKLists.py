b75d54f866fca6379c6f01c914ffe0765ebfa3d1
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-08 16:12:13 -0500

    completed merge k lists AGAIN

--- .idea/Interview150/Divide&Conquer/MergeKLists.py
+++ .idea/Interview150/Divide&Conquer/MergeKLists.py
@@ -8,24 +8,46 @@ class ListNode:
 class Solution:
 
     def mergeKLists(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
-        tempLists = lists;
-        minIndex = -1;
-        minVal = float('inf');
-        for index in range(len(tempLists)):
-            if tempLists[index] == None:
-                continue;
-            value = tempLists[index].val;
-            if minVal > value:
-                minVal = value;
-                minIndex = index;
-        if minIndex == -1:
+        newLst = [];
+        for lst in lists:
+            if lst != None:
+                newLst.append(lst)
+        return self.createList(newLst)
+    def createList(self, lists: list[Optional[ListNode]]) -> Optional[ListNode]:
+        if lists == None:
             return None;
-        tempLists[minIndex] = tempLists[minIndex].next;
-        nextList = self.mergeKLists(tempLists);
-        return ListNode(minVal, nextList);
+        lst = None;
+        current = lst;
+        while len(lists) != 0:
+            minIndex = 0;
+            minVal = lists[minIndex].val
+            for index in range(1, len(lists)):
+                val = lists[index].val
+                if val < minVal:
+                    minIndex = index;
+                    minVal = lists[index].val
+            if lst == None:
+                lst = ListNode(lists[minIndex].val);
+                current = lst;
+            else:
+                current.next = ListNode(lists[minIndex].val);
+                current = current.next;
+            if lists[minIndex].next == None:
+                lists = lists[:minIndex] + lists[minIndex+1:]
+            else:
+                lists[minIndex] = lists[minIndex].next;
+        return lst
 
-sol = Solution();
-lst1 = ListNode(1, ListNode(3, None));
-lst2 = ListNode(2);
-result = sol.mergeKLists([lst1, lst2]);
-print();
+
+
+sol = Solution()
+lst1_3 = ListNode(5);
+lst1_2 = ListNode(4, lst1_3);
+lst1 = ListNode(1, lst1_2);
+lst2_3 = ListNode(4);
+lst2_2 = ListNode(3, lst2_3)
+lst2 = ListNode(1, lst2_2)
+lst3_2 = ListNode(6);
+lst3 = ListNode(2, lst3_2);
+answer = sol.mergeKLists([lst1, lst2, lst3]);
+print()
