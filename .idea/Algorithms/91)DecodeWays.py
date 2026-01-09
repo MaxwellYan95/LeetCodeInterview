db42063f483914a96dee92a3fd717567c0c1a574
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-08 21:35:10 -0500

    looked at answer for Decode Ways

--- .idea/Algorithms/91)DecodeWays.py
+++ .idea/Algorithms/91)DecodeWays.py
@@ -2,36 +2,23 @@ from collections import defaultdict
 
 class Solution:
 
-    mapping = defaultdict(int)
+    mapping = defaultdict(int);
 
     def numDecodings(self, s: str) -> int:
-        if len(s) <= 2:
-            ways = self.twoDigits(s);
-            self.mapping[s] = ways;
-            return ways
-        middle = len(s)//2;
-        leftNum = self.numDecodings(s[:middle])
-        rightNum = self.numDecodings(s[middle:])
-        middleNum = s[middle-1: middle+1]
-        multi = self.twoDigits(middleNum)
-        result = 0;
-        if multi == 2:
-            result = self.mapping[s[:middle-1]] * self.mapping[s[middle+1:]]
-            + leftNum * rightNum
-        else:
-            result = leftNum * rightNum
-        self.mapping[s] = result;
-        return result;
-
-    def twoDigits(self, s: str):
-        number = int(s);
-        if number <= 26:
-            if number < 10:
-                return 1;
-            else:
-                return 2;
-        else:
-            return 1;
+        if len(s) == 0 or s[0] == '0':
+            return 0;
+        dp = [0 for i in range(len(s)+1)]
+        dp[0] = 1;
+        dp[1] = 1;
+        for i in range(2, len(s)+1):
+            oneDigit = int(s[i-1]);
+            twoDigit = int(s[i-2: i]);
+            if oneDigit != 0:
+                dp[i] += dp[i-1]
+            if 10 <= twoDigit <= 26:
+                dp[i] += dp[i-2]
+        return dp[len(s)];
 
 sol = Solution()
 print(sol.numDecodings("226"))
+print(sol.numDecodings("06"))
