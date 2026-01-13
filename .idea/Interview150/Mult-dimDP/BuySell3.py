7a4927e1cad2960a4243529e8e695de8026f478e
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-13 14:03:27 -0500

    completed BuySell3

--- .idea/Interview150/Mult-dimDP/BuySell3.py
+++ .idea/Interview150/Mult-dimDP/BuySell3.py
@@ -2,42 +2,28 @@ from collections import defaultdict;
 class Solution:
 
     def maxProfit(self, prices: list[int]) -> int:
-        size = len(prices);
-        n = len(prices);
-        # i3 --> transactions left (i3 == 2 means 2 transactions left)
-        # i2 --> you are allowed to buy or not (you cannot hold 2 stocks simultaneously)
-        # i1 --> sublist of prices
-        dp = [[[None for i3 in range(3)] for i2 in range(2)] for i1 in range(n)]
-        def dfs(i, can_buy, transactions_left):
-
-            if i == len(prices) or transactions_left == 0:
-                return 0
-
-            if dp[i][can_buy][transactions_left] is not None:
-                return dp[i][can_buy][transactions_left]
-
-            if can_buy:
-                ##here you can either buy or not buy
-                #if you buy, you subtract current price(cuz you paid) and recursively call sell
-                ##why not doing trancsation-1 in case of buy
-                ##Because a transaction is only completed when you SELL, not when you buy.
-                result =  max(
-                    -prices[i] + dfs(i+1, False, transactions_left),  # buy
-                    dfs(i+1, True, transactions_left)                 # not buy
-                )
+        dp = [[[-1 for i1 in range(0, 2)] for i2 in range(0, 3)] for i3 in range(len(prices))]
+        length = len(prices);
+        for transactions in range(0, 3):
+            dp[length-1][transactions][0] = 0;
+        for transactions in range(0, 3):
+            dp[length-1][transactions][1] = prices[length-1];
+        for day in range(length):
+            dp[day][2][0] = 0;
+        def recur(day: int, transNum: int, hasStock: int):
+            if dp[day][transNum][hasStock] != -1:
+                return dp[day][transNum][hasStock];
+            result = 0;
+            if hasStock == 0:
+                result = max(recur(day+1, transNum+1, 1) - prices[day],
+                             recur(day+1, transNum, hasStock));
             else:
-                result =  max(
-                    prices[i]+dfs(i+1, True, transactions_left-1),
-                    dfs(i+1, False, transactions_left)
-                )
+                result = max(recur(day+1, transNum, 0) + prices[day],
+                             recur(day+1, transNum, hasStock));
+            dp[day][transNum][hasStock] = result
+            return result;
 
-            dp[i][can_buy][transactions_left] = result
-            return result
+        return recur(0, 0, 0);
 
-        return dfs(0, True, 2)
-
-sol = Solution();
-grid = sol.createChart([3,3,5,0,0,3,1,4]);
-print(sol.maxProfit([3,3,5,0,0,3,1,4]));
-print(sol.maxProfit([1,2,3,4,5]));
-print(sol.maxProfit([1, 2]));
+sol = Solution()
+print(sol.maxProfit([3,3,5,0,0,3,1,4]))
