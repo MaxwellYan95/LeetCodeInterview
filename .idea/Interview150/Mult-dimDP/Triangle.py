214ba8f0090e900d5ea3c298a9255230a77f1ced
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-13 00:16:52 -0500

    completed triangle

--- .idea/Interview150/Mult-dimDP/Triangle.py
+++ .idea/Interview150/Mult-dimDP/Triangle.py
@@ -1,33 +1,18 @@
 class Solution:
 
-    dataDict = {};
+    def minimumTotal(self, triangle: list[list[int]]) -> int:
+        dp = [[-1 for i in range(len(triangle))] for j in range(len(triangle))]
+        col = 0;
+        for n in triangle[len(triangle)-1]:
+            dp[len(triangle)-1][col] = n;
+            col += 1;
+        col = 0;
+        for row in range(len(triangle)-2, -1, -1):
+            for c in range(0, len(triangle[row])):
+                dp[row][c] = triangle[row][c] + min(dp[row+1][c], dp[row+1][c+1]);
+        return dp[0][0]
 
-    def minimumTotal(self, triangle: List[List[int]]) -> int:
-        if len(triangle) == 1 and len(triangle[0]) == 1:
-            return triangle[0][0];
-        allZero = False;
-        left = [];
-        right = [];
-        for row in triangle[1:]:
-            left.append(tuple(row[:len(row)-1]));
-            right.append(tuple(row[1:]));
-        leftWay = 0;
-        rightWay = 0;
-        if tuple(left) in self.dataDict.keys():
-            leftWay = triangle[0][0] + self.dataDict[tuple(left)];
-        else:
-            newKey = tuple(left);
-            newValue = self.minimumTotal(left);
-            leftWay = triangle[0][0] + newValue;
-            self.dataDict[newKey] = newValue;
-        if tuple(right) in self.dataDict.keys():
-            rightWay = triangle[0][0] + self.dataDict[tuple(right)];
-        else:
-            newKey = tuple(right);
-            newValue = self.minimumTotal(right);
-            rightWay = triangle[0][0] + newValue;
-            self.dataDict[newKey] = newValue;
-        return min(leftWay, rightWay);
+
 
 
 
