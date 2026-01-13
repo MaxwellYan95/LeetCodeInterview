78d7c1dd4dd6e75556a85ba43c74c0fdffc28790
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-13 16:05:31 -0500

    looked at the solution for Unique BST

--- .idea/Algorithms/96)UniqueBSTree.py
+++ .idea/Algorithms/96)UniqueBSTree.py
@@ -1,19 +1,11 @@
 class Solution:
     def numTrees(self, n: int) -> int:
-        dp = [[-1 for i in range(n+1)] for i in range(n)];
-        for low in range(n):
-            dp[low][low+1] = 1;
-        for low in range(n-1):
-            dp[low][low+2] = 2;
-        for high in range(3, n+1):
-            low = 0
-            high2 = high
-            while high2 <= n:
-                dp[low][high2] = dp[low+1][high2] + dp[low][high2-1] + dp[low+1][high2-1];
-                low += 1;
-                high2 += 1;
-        return dp[0][n];
-
+        dp = [0 for i in range(n+1)];
+        dp[0] = dp[1] = 1;
+        for i in range(2, len(dp)):
+            for j in range(i):
+                dp[i] += (dp[j]*dp[i-1-j]);
+        return dp[n];
 
 sol = Solution();
 print(sol.numTrees(4))
