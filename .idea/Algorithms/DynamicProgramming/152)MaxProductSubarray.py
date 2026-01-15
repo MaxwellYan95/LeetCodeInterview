c35b7e9f13c777dbba78c02a79ba7e2d7e0d6a8c
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-15 15:08:19 -0500

    looked at solution of Max Product Subarray

--- .idea/Algorithms/DynamicProgramming/152)MaxProductSubarray.py
+++ .idea/Algorithms/DynamicProgramming/152)MaxProductSubarray.py
@@ -1,21 +1,19 @@
 class Solution:
     def maxProduct(self, nums: list[int]) -> int:
-        dp = [[-float('inf') for i in range(len(nums)+1)] for j in range(len(nums))]
-        allMulti = [[1 for i in range(len(nums)+1)] for j in range(len(nums))]
-        for i in range(len(nums)):
-            dp[i][i+1] = nums[i];
-        for i in range(len(nums)):
-            multi = 1;
-            for j in range(i+1, len(nums)+1):
-                multi *= nums[j-1];
-                allMulti[i][j] = multi;
-        def recur(low: int, high: int):
-            if dp[low][high] != -float('inf'):
-                return dp[low][high];
-            result = max(allMulti[low][high], recur(low+1, high), recur(low, high-1))
-            dp[low][high] = result;
-            return result
-        return recur(0, len(nums));
+        maxMulti = nums[0];
+        multi = 1;
+        for n in nums:
+            multi *= n;
+            maxMulti = max(multi, maxMulti);
+            if n == 0:
+                multi = 1;
+        multi = 1;
+        for n in nums[::-1]:
+            multi *= n;
+            maxMulti = max(multi, maxMulti);
+            if n == 0:
+                multi = 1;
+        return maxMulti;
 
 sol = Solution();
 print(sol.maxProduct([2,3,-2,4]))
