c6da78acd87c109b5389f5d81fc97f7153426fe7
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-16 17:50:57 -0500

    completed uniquePathsWithObstacles again

--- .idea/Interview150/Mult-dimDP/UniquePath2.py
+++ .idea/Interview150/Mult-dimDP/UniquePath2.py
@@ -1,24 +1,25 @@
 class Solution:
     def uniquePathsWithObstacles(self, obstacleGrid: list[list[int]]) -> int:
-        dp = [[-1 for j in obstacleGrid[0]] for i in obstacleGrid];
-        def recur(x, y) -> int:
-            if (obstacleGrid[y][x] == 1):
-                dp[y][x] = 0;
-                return 0;
-            if (x == len(obstacleGrid[0])-1 and y == len(obstacleGrid)-1):
-                dp[y][x] = 1;
-                return 1;
-            if dp[y][x] != -1:
-                return dp[y][x];
-            down = 0;
-            right = 0;
-            if y+1 <= len(obstacleGrid)-1:
-                down = recur(x, y+1);
-            if x+1 <= len(obstacleGrid[0])-1:
-                right = recur(x+1, y);
-            dp[y][x] = down + right;
-            return down + right;
-        return recur(0, 0);
-sol = Solution();
-print(sol.uniquePathsWithObstacles([[0,0,0],[0,1,0],[0,0,0]]));
+        m = len(obstacleGrid);
+        n = len(obstacleGrid[0]);
+        if obstacleGrid[m-1][n-1] == 1:
+            return 0;
+        dp = [[0 for i in range(n)] for j in range(m)];
 
+        for y in range(m-1, -1, -1):
+            if obstacleGrid[y][n-1] == 1:
+                break;
+            dp[y][n-1] = 1;
+        start = 1;
+        for x in range(n-1, -1, -1):
+            if obstacleGrid[m-1][x] == 1:
+                break;
+            dp[m-1][x] = 1;
+        for x in range(n-2, -1, -1):
+            for y in range(m-2, -1, -1):
+                if obstacleGrid[y][x] != 1:
+                    dp[y][x] = dp[y+1][x] + dp[y][x+1];
+        return dp[0][0]
+
+sol = Solution();
+print(sol.uniquePathsWithObstacles([[0,1,0,0]]))
