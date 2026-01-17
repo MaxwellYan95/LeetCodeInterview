3e6b72b0c729b9a05edccf496d2c463130f56e18
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-17 18:15:15 -0500

    looked at answer for longestPalindromeSubseq

--- .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
+++ .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
@@ -2,37 +2,17 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         if len(s) == 0:
             return 0;
-        dp = [["" for high in range(len(s)+1)] for low in range(len(s))]
-        dpLen = [[-1 for high in range(len(s)+1)] for low in range(len(s))]
-        for index in range(len(s)):
-            dp[index][index+1] = s[index];
-            dpLen[index][index+1] = 1;
-            dpLen[index][index] = 0;
-        def recur(low: int, high: int) -> str:
-            if dpLen[low][high] != -1:
-                return dp[low][high]
-            maxLen = 0;
-            maxStr = "";
-            for index in range(low, high):
-                length = 0;
-                string = "";
-                leftRecur = recur(low, index);
-                rightRecur = recur(index, high);
-                middle = s[index];
-                if leftRecur == rightRecur:
-                    string = leftRecur + "" + middle + "" + rightRecur;
-                elif len(leftRecur) > len(rightRecur):
-                    string = leftRecur;
+        n = len(s)
+        dp = [[0 for high in range(len(s)+1)] for low in range(len(s))]
+        for low in range(n-1, -1, -1):
+            dp[low][low+1] = 1;
+            for high in range(low+2, n+1):
+                if s[low] == s[high-1]:
+                    dp[low][high] = 2 + dp[low+1][high-1]
                 else:
-                    string = rightRecur;
-                length = len(string)
-                if length > maxLen:
-                    maxLen = length;
-                    maxStr = string;
-            dpLen[low][high] = maxLen;
-            dp[low][high] = maxStr;
-            return maxStr;
-        return len(recur(0, len(s)))
+                    dp[low][high] = max(dp[low+1][high], dp[low][high-1])
+        return dp[0][len(s)]
+
 
 
 sol = Solution()
