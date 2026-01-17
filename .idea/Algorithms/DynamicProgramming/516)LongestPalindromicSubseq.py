7beda29bcbc88f9a00e9cfe449f8e61a07234f57
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-17 14:07:49 -0500

    better solution 61 of 87 testcases

--- .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
+++ .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
@@ -2,20 +2,18 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         if len(s) == 0:
             return 0;
-        dp = [[-1 for high in range(len(s)+1)] for low in range(len(s))]
+        dp = {}
+        dp[""] = 0;
         for index in range(len(s)):
-            dp[index][index+1] = 1;
-            dp[index][index] = 0;
-        def recur(low: int, high: int):
-            if dp[low][high] != -1:
-                return dp[low][high]
-            midSub = recur(low+1, high-1);
+            dp[s[index]] = 1;
+        def recur(subStr: str):
+            if subStr in dp:
+                return dp[subStr];
             result = 0;
-            if s[low] == s[high-1] and midSub:
-                result = len(s[low:high])
-            else:
-                for lo in range(0, low):
-                    for hi in range(0, high):
-                        result = max(result, recur(lo, hi))
-            dp[low][high] = result;
+            for index in range(len(subStr)):
+                result = max(result, recur(subStr[:index] + "" + subStr[index+1:]))
+            midStr = subStr[1: len(subStr)-1];
+            if dp[midStr] == len(midStr) and subStr[0] == subStr[len(subStr)-1]:
+                result = len(subStr);
+            dp[subStr] = result;
             return result;
