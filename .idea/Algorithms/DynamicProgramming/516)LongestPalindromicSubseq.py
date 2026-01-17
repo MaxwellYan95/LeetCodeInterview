5fca5120937c622d47d0113daf2bcd4104f80b03
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-17 17:35:17 -0500

    attempting to solve longest palindromic subseq

--- .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
+++ .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
@@ -7,13 +7,27 @@ class Solution:
         for index in range(len(s)):
             dp[s[index]] = 1;
         def recur(subStr: str):
+            result = 0;
             if subStr in dp:
                 return dp[subStr];
-            result = 0;
-            for index in range(len(subStr)):
-                result = max(result, recur(subStr[:index] + "" + subStr[index+1:]))
-            midStr = subStr[1: len(subStr)-1];
-            if dp[midStr] == len(midStr) and subStr[0] == subStr[len(subStr)-1]:
-                result = len(subStr);
+            elif self.isPalindrone(subStr):
+                result = len(subStr)
+            else:
+                for index in range(len(subStr)):
+                    result = max(result, recur(subStr[:index] + "" + subStr[index+1:]))
             dp[subStr] = result;
             return result;
+        return recur(s)
+
+    def isPalindrone(self, s: str) -> bool:
+        low = 0;
+        high = len(s)-1
+        while low < high:
+            if s[low] != s[high]:
+                return False;
+            low += 1;
+            high -= 1;
+        return True;
+
+sol = Solution()
+print(sol.longestPalindromeSubseq("euazbipzncptldueeuechubrcourfpftcebikrxhybkymimgvldiwqvkszfycvqyvtiwfckexmowcxztkfyzqovbtmzpxojfofbvwnncajvrvdbvjhcrameamcfmcoxryjukhpljwszknhiypvyskmsujkuggpztltpgoczafmfelahqwjbhxtjmebnymdyxoeodqmvkxittxjnlltmoobsgzdfhismogqfpfhvqnxeuosjqqalvwhsidgiavcatjjgeztrjuoixxxoznklcxolgpuktirmduxdywwlbikaqkqajzbsjvdgjcnbtfksqhquiwnwflkldgdrqrnwmshdpykicozfowmumzeuznolmgjlltypyufpzjpuvucmesnnrwppheizkapovoloneaxpfinaontwtdqsdvzmqlgkdxlbeguackbdkftzbnynmcejtwudocemcfnuzbttcoew"))
