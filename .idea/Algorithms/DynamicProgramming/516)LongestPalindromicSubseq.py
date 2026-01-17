eef692e40adc803acb168b007da22687ff5c271f
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-17 17:59:34 -0500

    attempting to solve longest palindromic subseq again

--- .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
+++ .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
@@ -2,32 +2,38 @@ class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
         if len(s) == 0:
             return 0;
-        dp = {}
-        dp[""] = 0;
+        dp = [["" for high in range(len(s)+1)] for low in range(len(s))]
+        dpLen = [[-1 for high in range(len(s)+1)] for low in range(len(s))]
         for index in range(len(s)):
-            dp[s[index]] = 1;
-        def recur(subStr: str):
-            result = 0;
-            if subStr in dp:
-                return dp[subStr];
-            elif self.isPalindrone(subStr):
-                result = len(subStr)
-            else:
-                for index in range(len(subStr)):
-                    result = max(result, recur(subStr[:index] + "" + subStr[index+1:]))
-            dp[subStr] = result;
-            return result;
-        return recur(s)
+            dp[index][index+1] = s[index];
+            dpLen[index][index+1] = 1;
+            dpLen[index][index] = 0;
+        def recur(low: int, high: int) -> str:
+            if dpLen[low][high] != -1:
+                return dp[low][high]
+            maxLen = 0;
+            maxStr = "";
+            for index in range(low, high):
+                length = 0;
+                string = "";
+                leftRecur = recur(low, index);
+                rightRecur = recur(index, high);
+                middle = s[index];
+                if leftRecur == rightRecur:
+                    string = leftRecur + "" + middle + "" + rightRecur;
+                elif len(leftRecur) > len(rightRecur):
+                    string = leftRecur;
+                else:
+                    string = rightRecur;
+                length = len(string)
+                if length > maxLen:
+                    maxLen = length;
+                    maxStr = string;
+            dpLen[low][high] = maxLen;
+            dp[low][high] = maxStr;
+            return maxStr;
+        return len(recur(0, len(s)))
 
-    def isPalindrone(self, s: str) -> bool:
-        low = 0;
-        high = len(s)-1
-        while low < high:
-            if s[low] != s[high]:
-                return False;
-            low += 1;
-            high -= 1;
-        return True;
 
 sol = Solution()
 print(sol.longestPalindromeSubseq("euazbipzncptldueeuechubrcourfpftcebikrxhybkymimgvldiwqvkszfycvqyvtiwfckexmowcxztkfyzqovbtmzpxojfofbvwnncajvrvdbvjhcrameamcfmcoxryjukhpljwszknhiypvyskmsujkuggpztltpgoczafmfelahqwjbhxtjmebnymdyxoeodqmvkxittxjnlltmoobsgzdfhismogqfpfhvqnxeuosjqqalvwhsidgiavcatjjgeztrjuoixxxoznklcxolgpuktirmduxdywwlbikaqkqajzbsjvdgjcnbtfksqhquiwnwflkldgdrqrnwmshdpykicozfowmumzeuznolmgjlltypyufpzjpuvucmesnnrwppheizkapovoloneaxpfinaontwtdqsdvzmqlgkdxlbeguackbdkftzbnynmcejtwudocemcfnuzbttcoew"))
