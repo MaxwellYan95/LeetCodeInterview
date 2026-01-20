c8486bfd8f86b104b7f3976b59a64182645fea86
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-20 16:31:01 -0500

    attempted FlyCity again

--- .idea/MockInterview2_Jan20th/FlyCity.py
+++ .idea/MockInterview2_Jan20th/FlyCity.py
@@ -1,36 +1,30 @@
 class Solution:
-    def twoCitySchedCost(self, costs: List[List[int]]) -> int:
-        minA = costs[0][0]
-        minA_index = 0;
-        minB = costs[0][1]
-        minB_index = 0;
-        for index in range(len(cost)):
-            a = costs[index][0]
-            b = costs[index][1]
-            if a < minA:
-                minA = a;
-                minA_index = index;
-            if b < minB:
-                minB = b;
-                minB_index = index;
-        smallA = minA;
-        smallA_count = 0; # b flights
-        smallB = minB;
-        smallB_count = 0; # a flights
-        for flight in costs[:minA_index]+costs[minA_index+1:]:
-            a = flight[0]
-            b = flight[1]
-            if b < a:
-                smallA_count += 1;
-            smallA += min(a, b);
-        for flight in costs[:minB_index]+costs[minB_index+1:]:
-            a = flight[0]
-            b = flight[1]
-            if a < b:
-                smallB_count += 1;
-            smallB += min(a, b);
-        if smallA_count == 0:
-            return smallB;
-        if smallB_count == 0:
-            return smallA;
-        return min(smallA, smallB);
+    def twoCitySchedCost(self, costs: list[list[int]]) -> int:
+        def recur(costs: list[list[int]], isA: bool):
+            i2 = 1;
+            if isA:
+                i2 = 0
+            if len(costs) == 1:
+                return costs[0][i2]
+            minIndex = 0;
+            minVal = costs[0][i2]
+            secVal = costs[0][(i2+1) % 2]
+            for i1 in range(1, len(costs)):
+                c = costs[i1]
+                if minVal > c[i2]:
+                    minVal = c[i2];
+                    minIndex = i1;
+                    secVal = c[(i2+1) % 2]
+                if minVal == c[i2] and secVal > c[(i2+1) % 2]:
+                    minVal = c[i2];
+                    minIndex = i1;
+                    secVal = c[(i2+1) % 2]
+            subCost = costs[:minIndex] + costs[minIndex+1:];
+            return minVal + recur(subCost, not isA)
+        startA = recur(costs, True);
+        startB = recur(costs, False);
+        return min(startA, startB)
+
+
+sol = Solution()
+print(sol.twoCitySchedCost([[70,311],[74,927],[732,711],[126,583],[857,118],[97,928],[975,843],[175,221],[284,929],[816,602],[689,863],[721,888]]))
