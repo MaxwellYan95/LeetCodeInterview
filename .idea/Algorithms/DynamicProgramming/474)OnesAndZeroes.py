2d8ed2407a23bc966025a0421a2af08ec68b9aae
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-19 21:30:18 -0500

    looked at answer for ones and zeroes

--- .idea/Algorithms/DynamicProgramming/474)OnesAndZeroes.py
+++ .idea/Algorithms/DynamicProgramming/474)OnesAndZeroes.py
@@ -1,39 +1,23 @@
 class Solution:
     def findMaxForm(self, strs: list[str], m: int, n: int) -> int:
-        self.numMap = {};
-        self.maxMap = {};
+        dp = {(0,0): 0}
         for s in strs:
-            ones = 0;
-            zeroes = 0;
-            for char in s:
-                if char == '1':
-                    ones += 1;
-                else:
-                    zeroes += 1;
-            self.numMap[s] = [ones, zeroes];
-            if ones <= n and zeroes <= m:
-                self.maxMap[s] = 1;
-            else:
-                self.maxMap[s] = 0;
-        def recur(strLst: list[str]):
-            comboStr = "";
-            one = 0;
-            zero = 0;
-            for s in strLst:
-                one += self.numMap[s][0];
-                zero += self.numMap[s][1];
-                comboStr += (s + "");
-            self.numMap[comboStr] = [one, zero];
-            if comboStr in self.maxMap:
-                return self.maxMap[comboStr];
-            if one <= n and zero <= m:
-                self.maxMap[comboStr] = len(strLst);
-                return self.maxMap[comboStr];
-            result = 0;
-            for index in range(len(strLst)):
-                result = max(result, recur(strLst[:index] + strLst[index+1:]));
-            return result;
-        return recur(strs);
+            one = s.count("1");
+            zero = s.count("0")
+            newDp = {};
+            for (dpOne, dpZero), val in dp.items():
+                newOne = dpOne+one;
+                newZero = dpZero+zero;
+                if newOne <= n and newZero <= m:
+                    if (dpOne+one, dpZero+zero) not in dp:
+                        newDp[(dpOne+one, dpZero+zero)] = val+1;
+                    elif dp[(dpOne+one, dpZero+zero)] < val+1:
+                        newDp[(dpOne+one, dpZero+zero)] = val+1;
+            dp.update(newDp)
+        return max(dp.values())
+
+
+
 
 sol = Solution()
 res = sol.findMaxForm(["10","0001","111001","1","0"], 5, 3)
