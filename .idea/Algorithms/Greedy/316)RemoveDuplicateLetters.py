4e2c66e85011e43b37718d697f87bbb21af29946
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-21 18:33:16 -0500

    looked at the solution for 316) remove duplicate letters

--- .idea/Algorithms/Greedy/316)RemoveDuplicateLetters.py
+++ .idea/Algorithms/Greedy/316)RemoveDuplicateLetters.py
@@ -1,13 +1,21 @@
 class Solution:
     def removeDuplicateLetters(self, s: str) -> str:
-        minLetter = s;
-        result = minLetter + "";
-        for char in s[1:]:
-            if char < minLetter:
-                result = "" + char;
-                minLetter = char;
-            elif char not in result:
-                result += "" + char;
-        return result;
+        lastOccur = {};
+        stack = []; # stack is FIFO (First-In, First-Out)
+        visited = set();
+        for i in range(len(s)):
+            lastOccur[s[i]] = i;
+        for index in range(len(s)):
+            char = s[index]
+            if char not in visited:
+                # stack[-1] is the last element of the stack
+                while stack and stack[-1] > char and index < lastOccur[stack[-1]]:
+                    prev = stack.pop();
+                    visited.remove(prev);
+                stack.append(char);
+                visited.add(char);
+        return ''.join(stack)
+
+
 sol = Solution()
-print(sol.removeDuplicateLetters("abd"))
+print(sol.removeDuplicateLetters("acdbc"))
