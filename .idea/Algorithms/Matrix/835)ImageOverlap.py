869cf80e396103f2dbd2ae1e75ce55eca99e7fec
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-22 11:02:40 -0500

    looked at solution for Image Overlap

--- .idea/Algorithms/Matrix/835)ImageOverlap.py
+++ .idea/Algorithms/Matrix/835)ImageOverlap.py
@@ -1,10 +1,27 @@
+from collections import defaultdict
+
 class Solution:
     def largestOverlap(self, img1: list[list[int]], img2: list[list[int]]) -> int:
         n = len(img1)
 
-        # first Index is lower bound
-        dp = [[0 for i in range(n)] for j in range(n)]
+        coord1 = []
+        coord2 = []
+        transformations = defaultdict(int)
 
+        # Filter points having 1 for each matrix respectively.
+        for i in range(n):
+            for j in range(n):
+                if img1[i][j] == 1:
+                    coord1.append((i, j))
+                if img2[i][j] == 1:
+                    coord2.append((i, j))
 
-        downRight = self.largestOverlap(img1[:len(img1)-1][:len(img1[0])-1], img2[:len(img2)-1][:len(img2[0])-1])
-        upRight = self.largestOverlap(img1[:len(img1)-1][:len(img1[0])-1], img2[:len(img2)-1][:len(img2[0])-1])
+        # For every point in filtered A, calculate the
+        # linear transformation vector with all points of filtered B
+        # count the number of the pairs that have the same transformation vector
+        for c1 in coord1:
+            for c2 in coord2:
+                transformations[(c1[0]-c2[0], c1[1]-c2[1])] += 1;
+        if (len(transformations.values()) == 0):
+            return 0;
+        return sorted(transformations.values())[len(transformations.values())-1];
