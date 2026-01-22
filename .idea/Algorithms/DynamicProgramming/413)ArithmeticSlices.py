2d32baa441bb3aa3d48f3d6bfa5908f6288aaef7
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-22 13:21:55 -0500

    looked at the solution for 413) arithmetic slices

--- .idea/Algorithms/DynamicProgramming/413)ArithmeticSlices.py
+++ .idea/Algorithms/DynamicProgramming/413)ArithmeticSlices.py
@@ -2,16 +2,17 @@ from collections import defaultdict
 
 class Solution:
     def numberOfArithmeticSlices(self, nums: list[int]) -> int:
-        slices = defaultdict(int)
+        slices = []
         if len(nums) <= 1:
             return 0;
         for i in range(len(nums)-1):
-            slices[nums[i]-nums[i+1]] += 1;
-        for begin in range(len(nums)-2):
-            diff = nums[begin]-nums[begin+1]
-            for end in range(begin+2, len(nums)):
-                newDiff = nums[end-1]-nums[end]
-                if diff != newDiff:
-                    break;
-                else:
-                    slices[diff] += 1;
+            slices.append(nums[i]-nums[i+1]);
+        prevSlice = slices[0]
+        count = [0 for i in range(len(nums))]
+        for i in range(1, len(slices)):
+            if slices[i] == prevSlice:
+                count[i+1] = count[i]+1;
+            else:
+                count[i+1] = 0;
+            prevSlice = slices[i]
+        return sum(count)
