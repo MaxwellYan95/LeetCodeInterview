e8fb7e4ac8cb5f814bb0c4efe56c5c4d8e02bf6d
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-22 15:30:16 -0500

    solution to MaxProductSubarray

--- .idea/Algorithms/DynamicProgramming/152)MaxProductSubarray.py
+++ .idea/Algorithms/DynamicProgramming/152)MaxProductSubarray.py
@@ -1,16 +1,22 @@
 class Solution:
     def maxProduct(self, nums: list[int]) -> int:
-        dp = [-float('inf') for i in range(len(nums)+1)];
-        dp[0] = max(dp[0], nums[0])
-        multi = [[0 for i in range(len(nums)+1)] for j in nums]
-        for index in range(len(nums)):
-            multi[index][index+1] = nums[index];
-            dp[index+1] = max(dp[index+1], multi[index][index+1])
-        for low in range(len(nums)):
-            for high in range(low+2, len(nums)+1):
-                multi[low][high] = multi[low][high-1] * nums[high-1];
-                dp[high] = max(dp[high-1], dp[high], multi[low][high])
-        return dp[len(nums)];
+        # Using two pass method
+        # Forward iteration and THEN backward iteration
+        res = -float('inf')
+        maxMulti = 1;
+        for n in nums[0:]:
+            maxMulti *= n;
+            res = max(res, maxMulti);
+            if n == 0:
+                maxMulti = 1;
+        maxMulti = 1;
+        for n in reversed(nums):
+            maxMulti *= n;
+            res = max(res, maxMulti);
+            if n == 0:
+                maxMulti = 1;
+        return res;
+
 
 sol = Solution();
-print(sol.maxProduct([2,3,-2,4]))
+print(sol.maxProduct([3, -1, 4]))
