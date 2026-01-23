bea4a1e57825eb542c830f8b797be062cad49ba2
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-23 15:50:01 -0500

    attempted LargestNum.py (176 out of 236)

--- .idea/Algorithms/Greedy/179)LargestNum.py
+++ .idea/Algorithms/Greedy/179)LargestNum.py
@@ -1,16 +1,16 @@
 class Solution:
     def largestNumber(self, nums: list[int]) -> str:
-        def recur(nums: list[int], place: int):
-            if len(nums) == 1:
-                return nums;
-            mapping = defaultdict(list)
-            result = []
-            keyLst = set();
-            for n in nums:
-                curKey = n
-                for i in range(place):
-                    curKey = curKey % 10;
-                    mapping[curKey].append(n);
-                    keyLst.add(curKey);
-            for k in keyLst:
-                result.extend(mapping[k], place+1);
+        numStr = [str(n) for n in nums];
+        maxLen = 1;
+        numStr.sort(key=lambda x: x[len(x)-1])
+        numStr.sort(key=lambda x: x[0]);
+        numStr = reversed(numStr)
+        result = ""
+        for s in numStr:
+            result += "" + s;
+        return result;
+
+sol = Solution()
+print('x' > '9')
+print(sol.largestNumber([3,30,34,5,9]))
+
