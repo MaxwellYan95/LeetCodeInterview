6589397b74ad957db06eb2a24df24c2cbc5c9ed1
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-23 16:28:10 -0500

    solution to 179) LargestNum.py

--- .idea/Algorithms/Greedy/179)LargestNum.py
+++ .idea/Algorithms/Greedy/179)LargestNum.py
@@ -1,11 +1,21 @@
 class Solution:
     def largestNumber(self, nums: list[int]) -> str:
+        # You want to compare the first digit.
+        # If the first digit is the same, compare the second digit.
+        # If the second digit is the same, compare the third digit.
+        # Vice versa
+
+        # 30 has lower priority than 3.
+        # 34 has higher priority than 3,
+        # This is because the greater digit should always come first.
         numStr = [str(n) for n in nums];
-        maxLen = 1;
-        numStr.sort(key=lambda x: x[len(x)-1])
-        numStr.sort(key=lambda x: x[0]);
-        numStr = reversed(numStr)
+
+        # REMEMBER: 0 <= nums[i] <= 10^9
+        # That's why you are using "lambda x: x*10"
+        numStr.sort(key=lambda x: x*10, reverse=True);
         result = ""
+        if numStr[0] == "0":
+            return "0";
         for s in numStr:
             result += "" + s;
         return result;
