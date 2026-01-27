52816d701e3dccf33eee4c99fb0cfbf845db8cf8
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-01-27 13:49:15 -0500

    found solution to 860) Lemonade Change

--- .idea/Algorithms/Greedy/860)LemonChange.py
+++ .idea/Algorithms/Greedy/860)LemonChange.py
@@ -1,21 +1,31 @@
 import heapq
 
 class Solution:
-    def lemonadeChange(self, bills: List[int]) -> bool:
-        stack = []
-        heapq.heapify(stack);
+    def lemonadeChange(self, bills: list[int]) -> bool:
+        fives = 0
+        tens = 0
         for payment in bills:
-            curAmount = payment;
-            heapq.heappush(stack, curAmount)
-            while curAmount > 5:
-                curAmount -= heapq.heappop(stack);
-                heapq.heapify(stack);
-            if curAmount < 5:
-                return False;
+            if payment == 5:
+                fives += 1;
+            elif payment == 10:
+                if fives == 0:
+                    return False;
+                fives -= 1;
+                tens += 1;
+            elif payment == 20:
+                if fives == 0: # Previously was "fives == 0 or tens == 0"
+                    return False;
+                elif tens == 0 and fives < 3:
+                    return False;
+                elif tens == 0:
+                    fives -= 3;
+                else:
+                    fives -= 1;
+                    tens -= 1;
         return True;
 
 sol = Solution()
-print(sol.lemonadeChange([5,5,5,10,20]))
+print(sol.lemonadeChange([5,5,10,20,5,5,5,5,5,5,5,5,5,10,5,5,20,5,20,5]))
 
 
 
