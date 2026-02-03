6bd31b133cff2d32d1ea68dde0d58ea295338098
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-03 17:29:34 -0500

    tried using greedy

--- .idea/IBM_preparation/TooManyBooks.py
+++ .idea/IBM_preparation/TooManyBooks.py
@@ -6,16 +6,12 @@ def max_books_on_shelf(heights: list[int]):
     Returns:
         int: Maximum number of books that can be placed on the new shelf
     """
-    dp = [1 for i in range(len(heights))];
-    dp[0] = 1;
-    maximum = 1;
-    for index in range(1, len(heights)):
-        n1 = heights[index]
-        for inner in range(0, index):
-            n2 = heights[inner]
-            if n1 > n2:
-                dp[index] = max(dp[index], dp[inner]+1);
-                maximum = max(maximum, dp[index])
-    return maximum
+    maximum = 0;
+    for index in range(0, len(heights)-1):
+        cur = heights[index]
+        next = heights[index+1];
+        if cur < next:
+            maximum += 1;
+    return maximum+1
 
 print(max_books_on_shelf([10, 9, 2, 5, 3, 7, 101, 18]))
