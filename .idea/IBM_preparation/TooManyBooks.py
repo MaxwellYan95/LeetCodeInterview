d48f2f3c989ecb40f0d6a9d7f3f70c146722af99
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-03 17:24:39 -0500

    2nd attempt

--- .idea/IBM_preparation/TooManyBooks.py
+++ .idea/IBM_preparation/TooManyBooks.py
@@ -6,17 +6,16 @@ def max_books_on_shelf(heights: list[int]):
     Returns:
         int: Maximum number of books that can be placed on the new shelf
     """
-    dp = {};
-    dp[heights[0]] = 1;
+    dp = [1 for i in range(len(heights))];
+    dp[0] = 1;
     maximum = 1;
     for index in range(1, len(heights)):
-        num = heights[index]
-        dp[num] = 1;
-        keys = [k for k in dp]
-        for k in keys:
-            if num > k:
-                dp[num] = max(dp[num], dp[k] + 1);
-                maximum = max(maximum, dp[num])
+        n1 = heights[index]
+        for inner in range(0, index):
+            n2 = heights[inner]
+            if n1 > n2:
+                dp[index] = max(dp[index], dp[inner]+1);
+                maximum = max(maximum, dp[index])
     return maximum
 
 print(max_books_on_shelf([10, 9, 2, 5, 3, 7, 101, 18]))
