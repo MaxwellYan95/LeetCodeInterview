6be8b6549aee4ed99c48e32aff20d7673f5a4674
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-05 00:07:43 -0500

    I solved largest num again (sort() takes 2 parameters, key and reverse. I didn't search online. I used the method description.)

--- .idea/Algorithms/Greedy/179)LargestNum.py
+++ .idea/Algorithms/Greedy/179)LargestNum.py
@@ -3,10 +3,12 @@ class Solution:
         maxLen = 0;
         for n in nums:
             maxLen = max(len(str(n)), maxLen)
-        nums.sort(lambda x: str(x) * maxLen)
+        nums.sort(key = lambda x: str(x) * maxLen, reverse=True)
         result = ""
         for n in nums:
             result += str(n);
+        if result[0] == '0':
+            return str(0);
         return result
 
 sol = Solution()
