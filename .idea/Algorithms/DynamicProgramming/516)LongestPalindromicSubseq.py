6c8d824258eefdfa160ad932fa329ddc6727a8ea
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-05 17:25:39 -0500

    solved longest palin subseq in 11 minutes, again. Used: if dp[low][high] == -1

--- .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
+++ .idea/Algorithms/DynamicProgramming/516)LongestPalindromicSubseq.py
@@ -1,17 +1,20 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        if len(s) == 0:
-            return 0;
-        n = len(s)
-        dp = [[0 for high in range(len(s)+1)] for low in range(len(s))]
-        for low in range(n-1, -1, -1):
-            dp[low][low+1] = 1;
-            for high in range(low+2, n+1):
-                if s[low] == s[high-1]:
-                    dp[low][high] = 2 + dp[low+1][high-1]
-                else:
-                    dp[low][high] = max(dp[low+1][high], dp[low][high-1])
-        return dp[0][len(s)]
+        dp = [[-1 for i in range(len(s)+1)] for j in range(len(s))]
+        for index in range(len(s)):
+            dp[index][index+1] = 1;
+            dp[index][index] = 0;
+        for low in range(len(s)-1, -1, -1):
+            for high in range(low+1, len(s)+1):
+                if dp[low][high] == -1:
+                    result = 0;
+                    if s[low] == s[high-1]:
+                        result = dp[low+1][high-1] + 2;
+                    else:
+                        result = max(dp[low][high-1], dp[low+1][high])
+                    dp[low][high] = result;
+        return dp[0][len(s)];
+
 
 
 
