d87fba095cc865eb7fd8d1ec1d7f5c4f4807c96f
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-05 00:37:27 -0500

    attempting longest palindrone

--- .idea/Interview150/Mult-dimDP/LongPalin.py
+++ .idea/Interview150/Mult-dimDP/LongPalin.py
@@ -1,20 +1,27 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        for length in range(len(s), 0, -1):
-            for start in range(0, len(s)-length + 1):
-                if self.check(s[start: start+length]) == True:
-                    return s[start: start+length];
-        return "";
+        dp = [[-1 for i in range(len(s)+1)] for j in range(len(s))]
+        for index in range(len(s)):
+            dp[index][index+1] = s[index];
+            dp[index][index] = "";
+        def recur(low: int, high: int):
+            if dp[low][high] != -1:
+                return dp[low][high]
+            left = recur(low+1, high)
+            right = recur(low, high-1)
+            result = 0;
+            if s[low] == s[high-1] and dp[low+1][high-1] == s[low+1:high-1]:
+                result = s[low:high];
+            elif len(left) > len(right):
+                result = left
+            else:
+                result = right
+            dp[low][high] = result;
+            return dp[low][high]
+        return recur(0, len(s))
 
-    def check(self, s: str) -> bool:
-        left = 0;
-        right = len(s)-1;
-        while left<right:
-            if s[left] != s[right]:
-                return False;
-            left = left + 1;
-            right = right - 1;
-        return True;
+
+
 
 sol = Solution();
 print(sol.longestPalindrome("cbbd"));
