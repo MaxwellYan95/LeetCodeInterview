cff1ebb5d6562feda1822d40904f5105f7c6660c
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-07 16:52:07 -0500

    my final attempt at 542) Matrix01.py. Got 19 out of 50 testcases

--- .idea/Algorithms/DynamicProgramming/542)Matrix01.py
+++ .idea/Algorithms/DynamicProgramming/542)Matrix01.py
@@ -2,21 +2,31 @@ class Solution:
     def updateMatrix(self, mat: list[list[int]]) -> list[list[int]]:
         width = len(mat[0])
         height = len(mat)
-        dp = [[float('inf') for i in range(width)] for j in range(height)]
+        self.dp = mat.copy()
+        self.dir = [[0, -1], [0, 1], [-1, 0], [1, 0]]
+        self.visited = [[False for i in range(width)] for j in range(height)]
+
+        def recur(row: int, col: int):
+            if self.dp[row][col] == 0:
+                return self.dp[row][col];
+            if self.visited[row][col] == True:
+                return self.dp[row][col];
+            self.visited[row][col] = True
+            result = float('inf')
+            for x, y in self.dir:
+                newRow = row + x;
+                newCol = col + y;
+                if newCol >= 0 and newRow >= 0 \
+                        and newCol < width and newRow < height:
+                    result = min(result, recur(newRow, newCol)+1);
+            self.dp[row][col] = result
+            return self.dp[row][col]
+
         for row in range(height):
             for col in range(width):
-                if mat[row][col] == 0:
-                    dp[row][col] = 0
-        dir = [[0, -1], [0, 1], [-1, 0], [1, 0]]
-        for row in range(height):
-            for col in range(width):
-                if dp[row][col] == float('inf'):
-                    result = dp[row][col]
-                    for x, y in dir:
-                        newRow = row + x;
-                        newCol = col + y;
-                        if newCol >= 0 and newRow >= 0 \
-                            and newCol < width and newRow < height:
+                if self.visited[row][col] == False:
+                    recur(row,col)
+        return self.dp
 
 sol = Solution()
 print(sol.updateMatrix([[1,0,1,1,0,0,1,0,0,1],[0,1,1,0,1,0,1,0,1,1],[0,0,1,0,1,0,0,1,0,0],[1,0,1,0,1,1,1,1,1,1],[0,1,0,1,1,0,0,0,0,1],[0,0,1,0,1,1,1,0,1,0],[0,1,0,1,0,1,0,0,1,1],[1,0,0,0,1,1,1,1,0,1],[1,1,1,1,1,1,1,0,1,0],[1,1,1,1,0,1,0,0,1,1]]))
