ae112fbed4eb0fe574df25bd970e13dda4add9d4
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-06 23:33:06 -0500

    changed approach for 947) Most Stones Removed

--- .idea/Algorithms/Graphs/947)MostStonesRemoved.py
+++ .idea/Algorithms/Graphs/947)MostStonesRemoved.py
@@ -2,28 +2,21 @@ from collections import defaultdict
 
 class Solution:
     def removeStones(self, stones: list[list[int]]) -> int:
-        self.neighbors = defaultdict(list)
-        for i in range(len(stones)):
-            for j in range(i+1, len(stones)):
-                x1, y1 = stones[i]
-                x2, y2 = stones[j]
-                if x1 == x2 or y1 == y2:
-                    self.neighbors[(x1, y1)].append((x2, y2))
-                    self.neighbors[(x2, y2)].append((x1, y1))
-        self.visited = set();
-        def dfs(x: int, y: int):
-            if (x, y) in self.visited:
-                return 1;
-            self.visited.add((x,y))
-            if len(self.neighbors[(x,y)]) == 0:
-                return 1;
-            result = 0;
-            prevNeigh = self.neighbors[(x,y)]
-            for x2, y2 in prevNeigh:
-                result += dfs(x2, x1);
-            return result+1;
-        first = list(self.neighbors.keys())[0]
-        return dfs(first[0], first[1])-1
+        self.visited = [];
+        result = 0;
+        for index in range(len(stones)):
+            s1 = stones[index]
+            self.visited.append(s1)
+            if s1 in self.visited:
+                neighbor = 0;
+                recent = s1;
+                for s2 in stones[index+1:]:
+                    if recent[0] == s2[0] or recent[0] == s2[0]:
+                        neighbor += 1;
+                        recent = s2;
+                        self.visited.append(s2)
+                result += neighbor
+        return result
 
 sol = Solution()
 print(sol.removeStones([[0,0],[0,2],[1,1],[2,0],[2,2]]))
