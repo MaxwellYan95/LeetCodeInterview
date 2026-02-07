db9d01ecc25152c98d88c6ceaf0d045da1154f13
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-07 12:01:56 -0500

    completed matrix rotation again

--- .idea/Interview150/Matrix/Rotation.py
+++ .idea/Interview150/Matrix/Rotation.py
@@ -1,11 +1,10 @@
 def rotate(matrix: list[list[int]]) -> None:
-    """
-    Do not return anything, modify matrix in-place instead.
-    """
-    tempMatrix = [[matrix[(len(matrix)-1)-col][row] for col in range(len(matrix))]for row in range(len(matrix))]
-    matrix.replace(tempMatrix)
-    print(matrix)
+    tempMatrix = [[matrix[row][col] for col in range(len(matrix[0]))] for row in range(len(matrix))];
+    reverseMatrix = [row for row in tempMatrix[::-1]];
+    for col in range(len(matrix[0])):
+        for row in range(len(matrix)):
+            matrix[col][row] = reverseMatrix[row][col]
 
 m = [[1,2,3],[4,5,6],[7,8,9]]
 rotate(m)
-print(m)
+print()
