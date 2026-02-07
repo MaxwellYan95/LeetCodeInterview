8918252e8747f3a6f6b814863b0a38ba67111501
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-06 23:44:13 -0500

    found solution to 947) most stones removed. I should use connected components

--- .idea/Algorithms/Graphs/947)MostStonesRemoved.py
+++ .idea/Algorithms/Graphs/947)MostStonesRemoved.py
@@ -2,21 +2,32 @@ from collections import defaultdict
 
 class Solution:
     def removeStones(self, stones: list[list[int]]) -> int:
-        self.visited = [];
-        result = 0;
-        for index in range(len(stones)):
-            s1 = stones[index]
-            self.visited.append(s1)
-            if s1 in self.visited:
-                neighbor = 0;
-                recent = s1;
-                for s2 in stones[index+1:]:
-                    if recent[0] == s2[0] or recent[0] == s2[0]:
-                        neighbor += 1;
-                        recent = s2;
-                        self.visited.append(s2)
-                result += neighbor
-        return result
+        self.neighbors = defaultdict(list)
+        self.points = []
+        for i in range(len(stones)):
+            x1, y1 = stones[i]
+            for j in range(i+1, len(stones)):
+                x2, y2 = stones[j]
+                if x1 == x2 or y1 == y2:
+                    self.neighbors[(x1, y1)].append((x2, y2))
+                    self.neighbors[(x2, y2)].append((x1, y1))
+            self.points.append((x1, y1))
+        self.visited = set();
+        def dfs(x: int, y: int):
+            self.visited.add((x,y))
+            for x2, y2 in self.neighbors[(x,y)]:
+                if (x2, y2) not in self.visited:
+                    dfs(x2, y2)
+
+        component = 0;
+        for p in self.points:
+            if p not in self.visited:
+                dfs(p[0], p[1]);
+                component += 1;
+
+        # Find all connected components
+        # You have to keep 1 node from each connected component
+        return len(stones) - component
 
 sol = Solution()
 print(sol.removeStones([[0,0],[0,2],[1,1],[2,0],[2,2]]))
