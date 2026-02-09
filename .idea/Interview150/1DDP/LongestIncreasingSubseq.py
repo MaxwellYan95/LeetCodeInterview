0838f3cd9faba13f64d71b9ede9866d916b26d38
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-09 14:57:01 -0500

    solved Longest Increasing Subseq again

--- .idea/Interview150/1DDP/LongestIncreasingSubseq.py
+++ .idea/Interview150/1DDP/LongestIncreasingSubseq.py
@@ -1,11 +1,20 @@
+from collections import defaultdict
+
 class Solution:
     def lengthOfLIS(self, nums: list[int]) -> int:
-        results = [1 for n in nums];
-        for index in range(len(nums)-2, -1, -1):
-            for inner in range(index+1, len(nums)):
-                if nums[index] < nums[inner]:
-                    results[index] = max(1+results[inner], results[index]);
-        return max(results);
+        dp = defaultdict(int)
+        dp[nums[0]] = 1;
+        result = 1;
+        for index in range(len(nums)):
+            n = nums[index]
+            dp[n] = 1
+            for prev in nums[:index]:
+                if prev < n:
+                    dp[n] = max(dp[n], dp[prev] + 1);
+                    result = max(result, dp[n])
+        return result
+
+
 
 sol = Solution();
 print(sol.lengthOfLIS([10,9,2,5,3,7,101,18]));
