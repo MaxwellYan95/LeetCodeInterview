91b0947a04d0b33b6bb18aa98682b0e70b82e080
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-09 14:33:25 -0500

    solved Maximal Square again

--- .idea/Interview150/Mult-dimDP/MaximalSquare.py
+++ .idea/Interview150/Mult-dimDP/MaximalSquare.py
@@ -1,17 +1,17 @@
 class Solution:
     def maximalSquare(self, matrix: list[list[str]]) -> int:
+        # you are trying to find the maxSide
+        maxSide = 0
         dp = [[0 for i in range(len(matrix[0]))] for j in range(len(matrix))]
-        m = len(matrix[0])
-        n = len(matrix)
-        maxSide = 0;
-        for n2 in range(n):
-            for m2 in range(m):
-                if matrix[n2][m2] == "1":
-                    if n2 == 0 or m2 == 0:
-                        dp[n2][m2] = 1;
+        for row in range(len(matrix)):
+            for col in range(len(matrix[0])):
+                if matrix[row][col] == "1":
+                    if row == 0 or col == 0:
+                        dp[row][col] = 1;
                     else:
-                        dp[n2][m2] = min(dp[n2-1][m2], dp[n2][m2-1], dp[n2-1][m2-1]) + 1;
-                    maxSide = max(maxSide, dp[n2][m2])
+                        # you need to include dp[row-1][col-1] inside min()
+                        dp[row][col] = min(dp[row-1][col], dp[row][col-1], dp[row-1][col-1])+1
+                    maxSide = max(maxSide, dp[row][col]);
         return maxSide*maxSide;
 
 
