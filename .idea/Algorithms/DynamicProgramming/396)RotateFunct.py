292a6b8f3854c8c8bcc7f56a2219c0c7ce3b8418
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-12 23:13:29 -0500

    solved 396) solved rotate funct AGAIN

--- .idea/Algorithms/DynamicProgramming/396)RotateFunct.py
+++ .idea/Algorithms/DynamicProgramming/396)RotateFunct.py
@@ -1,14 +1,10 @@
 class Solution:
     def maxRotateFunction(self, nums: list[int]) -> int:
-        maxRes = -float('inf');
-        total = sum(nums)
-        n = len(nums)
-        res = 0;
-        for index in range(n):
-            res += (index*nums[index]);
-        maxRes = max(maxRes, res);
-        for index in range(n):
-            res += total;
-            res -= (n*nums[n-1-index]);
-            maxRes = max(maxRes, res);
-        return maxRes
+        maxResult = sum([nums[index]*index for index in range(len(nums))])
+        num = maxResult;
+        allNum = sum(nums)
+        for n in reversed(nums):
+            num += allNum;
+            num -= len(nums)*n;
+            maxResult = max(maxResult, num);
+        return maxResult;
