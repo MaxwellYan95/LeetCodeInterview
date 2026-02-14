56c6fbac0f7bf411fe5820340cb047a47cc1d449
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-13 19:53:51 -0500

    deleted a file

--- .idea/Interview75/DFS/GoodNodes.py
+++
@@ -1,7 +0,0 @@
-# Definition for a binary tree node.
-class ListNode:
-    def __init__(self, val=0, next=None):
-        self.val = val
-        self.next = next
-class Solution:
-    def goodNodes(self, root: TreeNode) -> int:
