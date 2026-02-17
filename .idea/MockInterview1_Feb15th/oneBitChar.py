98380c60b1815363d20c36985d897c2e13e0ac10
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-02-17 13:42:57 -0500

    Solution to oneBitChar. if bit is 1, skip 2 indexes. You cannot just have a single 1. But you can have a single 0.

--- .idea/MockInterview1_Feb15th/oneBitChar.py
+++ .idea/MockInterview1_Feb15th/oneBitChar.py
@@ -2,15 +2,16 @@ class Solution:
 
     # PAY ATTENTION to last bit. It's a 0
     def isOneBitCharacter(self, bits: list[int]) -> bool:
-        return self.secondLastIsOne(bits) and self.secondLastIsOne(bits[1:])
-
-    def secondLastIsOne(self, bits: list[int]):
-        if len(bits) == 0:
-            return True;
         index = 0;
-        for i in range(0, len(bits), 2):
-            index = i;
-        return bits[index] != 1;
+        result = False;
+        while index < len(bits):
+            if bits[index] == 1:
+                index += 2;
+                result = False;
+            else:
+                index += 1;
+                result = True;
+        return result
 
 sol = Solution()
 print(sol.isOneBitCharacter([0]))
