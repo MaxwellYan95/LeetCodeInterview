ed0de451d5a74f983fd3bc70a6334f51525354e3
MaxwellYan95 <dvd.maxyanyan@gmail.com>
2026-03-10 13:55:47 -0400

    solved nearest exit

--- .idea/Interview75/BFS/NearestExit.py
+++ .idea/Interview75/BFS/NearestExit.py
@@ -1,16 +1,16 @@
+from collections import deque
+
 class Solution:
     def nearestExit(self, maze: list[list[str]], entrance: list[int]) -> int:
         dir = [[-1, 0], [0, -1], [1, 0], [0, 1]]
-        stack = [entrance];
-        visited = [[False for i in range(len(maze[0]))] for j in range(len(maze))]
+        stack = deque([entrance]);
+        maze[entrance[0]][entrance[1]] = '+'
         result = 0;
         while stack:
-            n = len(stack)
+            n = len(stack);
             result += 1;
-            stackCopy = stack.copy();
-            stack = [];
-            for x, y in stackCopy:
-                visited[x][y] = True;
+            for i in range(n):
+                x, y = stack.popleft();
                 for dx, dy in dir:
                     newX = x - dx;
                     newY = y - dy;
@@ -19,11 +19,10 @@ class Solution:
                         continue;
                     if maze[newX][newY] == '+':
                         continue;
-                    if visited[newX][newY]:
-                        continue;
                     if newX == 0 or newY == 0 or newX == len(maze)-1 or newY == len(maze[0])-1:
                         return result;
                     stack.append([newX, newY]);
+                    maze[newX][newY] = '+'
         return -1
 
 sol = Solution()
